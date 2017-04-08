7f7396f857743cb9ec40d7316d5e99641cbd782e
antioch-git-user <git-user@antioch.jp>
2017-04-08 22:27:56 +0900

    first commit: tokyo antioch site
    top page index.html
    2016 directory

---
+++ 2016/include/new_pickup.php
@@ -0,0 +1,62 @@
+<div class="column-inner">
+<span class="label-1">ブログ</span>
+<span>NEW</span>
+<a href="http://antiochblog.jp/info/1609funabashi" target="_blank"><img src="/2016/images/top/1609funabashi.jpg"></a>
+<p class="block-date">2016年9月9日</p>
+<p class="block-title"><a href="http://antiochblog.jp/info/1609funabashi" target="_blank">初秋の船橋を行く<br>関東宣教の旅<br>Giftsもコンサート<br>(聖会：2016年9月9日)</a></p>
+</div>
+
+<div class="column-inner">
+<span class="label-1">ブログ</span>
+<span>NEW</span>
+<a href="http://astone.tv/events/1609compo/" target="_blank"><img src="/2016/images/top/1609sakushiyorokobiantioch.jpg"></a>
+<p class="block-date">2016年9月5日〜11日</p>
+<p class="block-title"><a href="http://astone.tv/events/1609compo/" target="_blank">風のひびき4集77曲決まる！<br>喜びと悲しみの一週間<br>東京アンテオケ教会版</a></p>
+</div>
+
+<div class="column-inner">
+<span class="label-1">ブログ</span>
+<span>NEW</span>
+<a href="http://antiochblog.jp/praise/1609ikebukuro/" target="_blank"><img src="/2016/images/top/1609ikebukuro.jpg"></a>
+<p class="block-date">2016年9月9日〜10日</p>
+<p class="block-title"><a href="http://antiochblog.jp/praise/1609ikebukuro/" target="_blank">池袋西口公園<br>賛美チームライブ<br>(日程：2016年9月10日)</a></p>
+</div>
+
+<div class="column-inner">
+<span class="label-1">ブログ</span>
+<span>NEW</span>
+<a href="http://antiochblog.jp/mission/cambodia201609/" target="_blank"><img src="/2016/images/top/1609cambodiacountdown.jpg"></a>
+<p class="block-date">2016年9月5日〜11日</p>
+<p class="block-title"><a href="http://antiochblog.jp/mission/cambodia201609/" target="_blank">カンボジアチーム<br>直前情報写真集<br>(チーム日程：2016年9月12日〜14日)</a></p>
+</div>
+
+<div class="column-inner">
+<span class="label-1">ブログ</span>
+<span>NEW</span>
+<a href="http://astone.tv/events/1609healing/" target="_blank"><img src="/2016/images/top/1609iyashinotsudoi.jpg"></a>
+<p class="block-date">2016年9月9日〜10日</p>
+<p class="block-title"><a href="http://astone.tv/events/1609healing/" target="_blank">9月いやしの集い<br>(日程：2016年9月10日)</a></p>
+</div>
+
+<div class="column-inner">
+<span class="label-1">ブログ</span>
+<span>NEW</span>
+<a href="http://astone.tv/events/77composition2016009/" target="_blank"><img src="/2016/images/top/1609sakushiyorokobizenkoku.jpg"></a>
+<p class="block-date">2016年9月5日〜11日</p>
+<p class="block-title"><a href="http://astone.tv/events/77composition2016009/" target="_blank">風のひびき4集77曲決まる！<br>喜びと悲しみの一週間<br>全国各教会版</a></p>
+</div>
+
+<div class="column-inner">
+<span class="label-2">映像</span>
+<span>NEW</span>
+<a href="http://tv.antioch.jp/digest/201608israel_cm/" target="_blank"><img src="/2016/images/top/1608israelteaminfo_m.jpg"></a>
+<p class="block-title"><a href="http://tv.antioch.jp/digest/201608israel_cm/" target="_blank">イスラエルチーム紹介映像<br>(日程：2016年10月31日〜11月12日)<br>(再生時間：03:46)</a></p>
+</div>
+
+<!--<div class="column-inner">
+<span class="label-2">映像</span>
+   <span>NEW</span>
+   <a href="https://www.youtube.com/watch?annotation_id=57b0a090-0000-29cc-931f-001a113f69e8&feature=iv&src_vid=AmCM3OlEq-c&v=B8vpXDP0T5A" target="_blank" onclick="javascript:pageTracker._trackPageview('上野J.GOSPEL FES 生中継'); "><img src="/2016/images/top/1608uenofeslive.png"></a>
+   <p class="block-date">2016年9月3日</p>
+   <p class="block-title"><a href="https://www.youtube.com/watch?annotation_id=57b0a090-0000-29cc-931f-001a113f69e8&feature=iv&src_vid=AmCM3OlEq-c&v=B8vpXDP0T5A" target="_blank" onclick="javascript:pageTracker._trackPageview('上野J.GOSPEL FES 生中継');">上野 J.GOSPEL FES 生中継<br> (11:30スタート)<br>『The Vision のライブストリーム』のタイトルで、『〜人が視聴中』となっているのが、ライブ映像です。見つからない場合は下をスクロールしてください。</a></p>
+</div>-->
