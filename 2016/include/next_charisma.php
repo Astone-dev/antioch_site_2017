e5b5b140d4b22157f03fc02a4fe0bd97827532d5
fujita-PC\fujita <johnfuji4845@gmail.com>
2017-06-04 22:16:56 +0900

    カリスマ聖会　日付変更

--- 2016/include/next_charisma.php
+++ 2016/include/next_charisma.php
@@ -3,10 +3,10 @@
 // ""の間のみ編集可能。他の箇所を触れるとWEBが壊れるため注意！ 
 // 例1: $next_charisma_date = "2016年7月3日(日)";
 // 例2: $next_charisma_date = "2017年12月11日(日)";
-$next_charisma_date = "2017年 6月4日(日)";
+$next_charisma_date = "2017年 7月2日(日)";
 
 
 // 英語版 のカリスマ聖会の年月日を記載
 // 例: $next_charisma_date_eng = " Aug 21st, Sun.";
-$next_charisma_date_eng = "Jun. 4th, Sun.";
+$next_charisma_date_eng = "Jul. 2nd, Sun.";
 ?>
