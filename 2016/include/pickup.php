19ea07bb6bd0d39e2d44eeff717755cecf3580d4
timo77 <timo77@antioch.jp>
2017-06-04 18:19:42 +0900

    ・グレーシャスドアーのブログを下に下げる

--- 2016/include/pickup.php
+++ 2016/include/pickup.php
@@ -57,14 +57,6 @@
 <div class="column-inner">
 <span class="label-1">ブログ</span>
 <span>NEW</span>
-<a href="http://antiochblog.jp/praise/1706day/" target="_blank"><img src="/2016/images/top/1704gracious.jpg"></a>
-<p class="block-date">2017年6月3日</p>
-<p class="block-title"><a href="http://antiochblog.jp/praise/1706day/" target="_blank">GRACIOUS DOORS<br>都内の施設でコンサート<br>(日程：2017年6月3日)</a></p>
-</div>
-
-<div class="column-inner">
-<span class="label-1">ブログ</span>
-<span>NEW</span>
 <a href="http://antiochblog.jp/praise/mission1706/" target="_blank"><img src="/2016/images/top/1705missionweek.jpg"></a>
 <p class="block-date">2017年5月29日〜6月4日</p>
 <p class="block-title"><a href="http://antiochblog.jp/praise/mission1706/" target="_blank">ミッションスタッフと宣教師の一週間</a></p>
@@ -116,6 +108,14 @@
 <a href="http://kazenohibiki.blogspot.jp" target="_blank"><img src="/2016/images/top/1604kazenohibikiradio.jpg"></a>
 <p class="block-date">毎日19:00更新</p>
 <p class="block-title"><a href="http://kazenohibiki.blogspot.jp" target="_blank">インターネットラジオ<br>ゴスペル風のひびき<br>心に触れるゴスペルと楽しいおしゃべり 取っ替え引っ替え14組のパーソナリティー</a></p>
+</div>
+
+<div class="column-inner">
+<span class="label-1">ブログ</span>
+<span>NEW</span>
+<a href="http://antiochblog.jp/praise/1706day/" target="_blank"><img src="/2016/images/top/1704gracious.jpg"></a>
+<p class="block-date">2017年6月3日</p>
+<p class="block-title"><a href="http://antiochblog.jp/praise/1706day/" target="_blank">GRACIOUS DOORS<br>都内の施設でコンサート<br>(日程：2017年6月3日)</a></p>
 </div>
 
 <div class="column-inner">
