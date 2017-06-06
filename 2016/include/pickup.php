fcdf4d2a06e77ce23da87853cf207cf422639b62
Samuel N Kasahara <samuel.livebyfaith@gmail.com>
2017-06-07 01:37:17 +0900

    PICKUPのGraceと宝石を下げる

--- 2016/include/pickup.php
+++ 2016/include/pickup.php
@@ -57,14 +57,6 @@
 <div class="column-inner">
 <span class="label-1">ブログ</span>
 <span>NEW</span>
-<a href="http://antiochblog.jp/praise/fuchu1706/" target="_blank"><img src="/2016/images/top/1706graceshisetsude.jpg"></a>
-<p class="block-date">2017年6月6日</p>
-<p class="block-title"><a href="http://antiochblog.jp/praise/fuchu1706/" target="_blank">GRACEと宝石<br>府中市の特別養護老人ホームでゴスペルコンサート　</a></p>
-</div>
-
-<div class="column-inner">
-<span class="label-1">ブログ</span>
-<span>NEW</span>
 <a href="http://antiochblog.jp/praise/1706rainy" target="_blank"><img src="/2016/images/top/1706nations.jpg"></a>
 <p class="block-date">2017年6月5日〜6月11日</p>
 <p class="block-title"><a href="http://antiochblog.jp/praise/1706rainy" target="_blank">賛美隊梅雨の路上ライブに展開<br>全国各教会</a></p>
@@ -115,6 +107,13 @@
 <a href="http://kazenohibiki.blogspot.jp" target="_blank"><img src="/2016/images/top/1604kazenohibikiradio.jpg"></a>
 <p class="block-date">毎日19:00更新</p>
 <p class="block-title"><a href="http://kazenohibiki.blogspot.jp" target="_blank">インターネットラジオ<br>ゴスペル風のひびき<br>心に触れるゴスペルと楽しいおしゃべり 取っ替え引っ替え14組のパーソナリティー</a></p>
+</div>
+
+<div class="column-inner">
+<span class="label-1">ブログ</span>
+<a href="http://antiochblog.jp/praise/fuchu1706/" target="_blank"><img src="/2016/images/top/1706graceshisetsude.jpg"></a>
+<p class="block-date">2017年6月6日</p>
+<p class="block-title"><a href="http://antiochblog.jp/praise/fuchu1706/" target="_blank">GRACEと宝石<br>府中市の特別養護老人ホームでゴスペルコンサート　</a></p>
 </div>
 
 <div class="column-inner">
