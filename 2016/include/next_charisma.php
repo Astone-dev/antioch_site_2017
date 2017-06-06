04e7eb76160c44097a57a99a49e57e233d403389
Timothy Obata <timo77@antioch.jp>
2017-06-06 22:08:21 +0900

    test2

--- 2016/include/next_charisma.php
+++ 2016/include/next_charisma.php
@@ -1,7 +1,7 @@
 <?php
 //   ここにカリスマ聖会の年月日を記載
 // ""の間のみ編集可能。他の箇所を触れるとWEBが壊れるため注意！ 
-// 例1: $next_charisma_date = "2016年7月4日(日)";
+// 例1: $next_charisma_date = "2016年7月5日(日)";
 // 例2: $next_charisma_date = "2017年12月11日(日)";
 $next_charisma_date = "2017年 7月2日(日)";
 
