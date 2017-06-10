4a496557f623a4a8c3ef6a573b83e3cfac7f9ce2
fujita-PC\fujita <johnfuji4845@gmail.com>
2017-06-10 21:57:06 +0900

    6/11用バナーに変更

--- 2016/include/pickup.php
+++ 2016/include/pickup.php
@@ -14,13 +14,13 @@
 <p class="block-title"><a href="https://www.youtube.com/watch?v=nIJ1EyOVzTo" target="_blank" onclick="javascript:pageTracker._trackPageview('J.GOSPELチャリティーコンサート生中継');">吉祥寺J.GOSPEL FES生中継<br> (日程：2017年5月20日 11:00〜)</a></p>
 </div>-->
 
-<!--<div class="column-inner">
+<div class="column-inner">
 <span class="label-1">ブログ</span>
 <span>NEW</span>
 <a href="http://antiochblog.jp/mission/shikoku1706/" target="_blank"><img src="/2016/images/top/1706shikoku.jpg"></a>
 <p class="block-date">2017年6月11日〜6月15日</p>
 <p class="block-title"><a href="http://antiochblog.jp/mission/shikoku1706/" target="_blank">四国聖会</a></p>
-</div>-->
+</div>
 
 <div class="column-inner">
 <span class="label-1">ブログ</span>
@@ -81,34 +81,10 @@
 <div class="column-inner">
 <span class="label-1">ブログ</span>
 <span>NEW</span>
-<a href="http://astone.tv/events/euqaristias/20170610day/" target="_blank"><img src="/2016/images/top/1706euqarishinjyuku.jpg"></a>
-<p class="block-date">2017年6月6月10日</p>
-<p class="block-title"><a href="http://astone.tv/events/euqaristias/20170610day/" target="_blank">エウカリスティアス<br>新宿のデイサービスでゴスペル</a></p>
-</div>
-
-<div class="column-inner">
-<span class="label-1">ブログ</span>
-<span>NEW</span>
-<a href="http://astone.tv/events/1706arakawa/" target="_blank"><img src="/2016/images/top/1706labeiyuarakawa.jpg"></a>
-<p class="block-date">2017年6月10日</p>
-<p class="block-title"><a href="http://astone.tv/events/1706arakawa/" target="_blank">ラベイユ<br>荒川の施設でゴスペルコンサート</a></p>
-</div>
-
-<!--<div class="column-inner">
-<span class="label-1">ブログ</span>
-<span>NEW</span>
-<a href="http://antiochblog.jp/praise/uranos2017/category/1706nerima/" target="_blank"><img src="/2016/images/top/1706uranos.jpg"></a>
-<p class="block-date">2017年6月11日</p>
-<p class="block-title"><a href="http://antiochblog.jp/praise/uranos2017/category/1706nerima/" target="_blank">ウラノス<br>練馬のデイサービスでゴスペルコンサート</a></p>
-</div>
-
-<div class="column-inner">
-<span class="label-1">ブログ</span>
-<span>NEW</span>
 <a href="http://astone.tv/events/2017byhisspirit/20170611concert/" target="_blank"><img src="/2016/images/top/1706byhis.jpg"></a>
 <p class="block-date">2017年6月11日</p>
 <p class="block-title"><a href="http://astone.tv/events/2017byhisspirit/20170611concert/" target="_blank">By His spirit<br>世田谷の高齢者施設でゴスペルコンサート</a></p>
-</div>-->
+</div>
 
 <div class="column-inner">
 <span class="label-1">ブログ</span>
@@ -178,6 +154,27 @@
 <a href="http://kazenohibiki.blogspot.jp" target="_blank"><img src="/2016/images/top/1604kazenohibikiradio.jpg"></a>
 <p class="block-date">毎日19:00更新</p>
 <p class="block-title"><a href="http://kazenohibiki.blogspot.jp" target="_blank">インターネットラジオ<br>ゴスペル風のひびき<br>心に触れるゴスペルと楽しいおしゃべり 取っ替え引っ替え14組のパーソナリティー</a></p>
+</div>
+
+<div class="column-inner">
+<span class="label-1">ブログ</span>
+<a href="http://astone.tv/events/euqaristias/20170610day/" target="_blank"><img src="/2016/images/top/1706euqarishinjyuku.jpg"></a>
+<p class="block-date">2017年6月6月10日</p>
+<p class="block-title"><a href="http://astone.tv/events/euqaristias/20170610day/" target="_blank">エウカリスティアス<br>新宿のデイサービスでゴスペル</a></p>
+</div>
+
+<div class="column-inner">
+<span class="label-1">ブログ</span>
+<a href="http://astone.tv/events/1706arakawa/" target="_blank"><img src="/2016/images/top/1706labeiyuarakawa.jpg"></a>
+<p class="block-date">2017年6月10日</p>
+<p class="block-title"><a href="http://astone.tv/events/1706arakawa/" target="_blank">ラベイユ<br>荒川の施設でゴスペルコンサート</a></p>
+</div>
+
+<div class="column-inner">
+<span class="label-1">ブログ</span>
+<a href="http://antiochblog.jp/praise/uranos2017/category/1706nerima/" target="_blank"><img src="/2016/images/top/1706uranos.jpg"></a>
+<p class="block-date">2017年6月11日</p>
+<p class="block-title"><a href="http://antiochblog.jp/praise/uranos2017/category/1706nerima/" target="_blank">ウラノス<br>練馬のデイサービスでゴスペルコンサート</a></p>
 </div>
 
 <div class="column-inner">
