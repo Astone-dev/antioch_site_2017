f1e06924ef1c0ef9385952948fd1e1d5fea1a6da
Samuel N Kasahara <samuel.livebyfaith@gmail.com>
2017-06-13 12:14:59 +0900

    PICKUPエルサレムブログ追加

--- 2016/include/pickup.php
+++ 2016/include/pickup.php
@@ -57,9 +57,9 @@
 <div class="column-inner">
 <span class="label-1">ブログ</span>
 <span>NEW</span>
-<a href="http://antiochblog.jp/mission/1706patlis" target="_blank"><img src="/2016/images/top/1706kawaguchi.jpg"></a>
-<p class="block-date">2017年6月12日</p>
-<p class="block-title"><a href="http://antiochblog.jp/mission/1706patlis" target="_blank">PATOLIS<br>川口の病院の音楽会でゴスペルコンサート</a></p>
+<a href="http://antiochblog.jp/info/jerusalem2017/201706misato/" target="_blank"><img src="/2016/images/top/1706jerumisato.jpg"></a>
+<p class="block-date">2017年6月13日</p>
+<p class="block-title"><a href="http://antiochblog.jp/info/jerusalem2017/201706misato/" target="_blank">エルサレム<br>三郷のカフェでゴスペルコンサート</a></p>
 </div>
 
 <div class="column-inner">
@@ -122,6 +122,13 @@
 <a href="http://kazenohibiki.blogspot.jp" target="_blank"><img src="/2016/images/top/1604kazenohibikiradio.jpg"></a>
 <p class="block-date">毎日19:00更新</p>
 <p class="block-title"><a href="http://kazenohibiki.blogspot.jp" target="_blank">インターネットラジオ<br>ゴスペル風のひびき<br>心に触れるゴスペルと楽しいおしゃべり 取っ替え引っ替え14組のパーソナリティー</a></p>
+</div>
+
+<div class="column-inner">
+<span class="label-1">ブログ</span>
+<a href="http://antiochblog.jp/mission/1706patlis" target="_blank"><img src="/2016/images/top/1706kawaguchi.jpg"></a>
+<p class="block-date">2017年6月12日</p>
+<p class="block-title"><a href="http://antiochblog.jp/mission/1706patlis" target="_blank">PATOLIS<br>川口の病院の音楽会でゴスペルコンサート</a></p>
 </div>
 
 <div class="column-inner">
