5a2200e3b393498987a09be22bc39a46f2121120
Samuel N Kasahara <samuel.livebyfaith@gmail.com>
2017-06-15 17:43:03 +0900

    PICKUP修正 四国聖会下げ

--- 2016/include/pickup.php
+++ 2016/include/pickup.php
@@ -14,14 +14,6 @@
 <p class="block-title"><a href="https://www.youtube.com/watch?v=nIJ1EyOVzTo" target="_blank" onclick="javascript:pageTracker._trackPageview('J.GOSPELチャリティーコンサート生中継');">吉祥寺J.GOSPEL FES生中継<br> (日程：2017年5月20日 11:00〜)</a></p>
 </div>-->
 
-<div class="column-inner">
-<span class="label-1">ブログ</span>
-<span>NEW</span>
-<a href="http://antiochblog.jp/mission/shikoku1706/" target="_blank"><img src="/2016/images/top/1706shikoku.jpg"></a>
-<p class="block-date">2017年6月11日〜6月15日</p>
-<p class="block-title"><a href="http://antiochblog.jp/mission/shikoku1706/" target="_blank">向暑季節四国を行く<br>四国宣教の旅<br>(聖会日程：2017年6月12日〜14日)</a></p>
-</div>
-
 <!--<div class="column-inner">
 <span class="label-1">ブログ</span>
 <span>NEW</span>
@@ -116,6 +108,13 @@
 <a href="http://kazenohibiki.blogspot.jp" target="_blank"><img src="/2016/images/top/1604kazenohibikiradio.jpg"></a>
 <p class="block-date">毎日19:00更新</p>
 <p class="block-title"><a href="http://kazenohibiki.blogspot.jp" target="_blank">インターネットラジオ<br>ゴスペル風のひびき<br>心に触れるゴスペルと楽しいおしゃべり 取っ替え引っ替え14組のパーソナリティー</a></p>
+</div>
+
+<div class="column-inner">
+<span class="label-1">ブログ</span>
+<a href="http://antiochblog.jp/mission/shikoku1706/" target="_blank"><img src="/2016/images/top/1706shikoku.jpg"></a>
+<p class="block-date">2017年6月11日〜6月15日</p>
+<p class="block-title"><a href="http://antiochblog.jp/mission/shikoku1706/" target="_blank">向暑季節四国を行く<br>四国宣教の旅<br>(聖会日程：2017年6月12日〜14日)</a></p>
 </div>
 
 <div class="column-inner">
