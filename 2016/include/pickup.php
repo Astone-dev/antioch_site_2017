f923e70c9cc54a37508d2a2863c32758ffaddbf2
Samuel N Kasahara <samuel.livebyfaith@gmail.com>
2017-06-15 11:51:44 +0900

    PICKUP修正

--- 2016/include/pickup.php
+++ 2016/include/pickup.php
@@ -62,13 +62,13 @@
 <p class="block-title"><a href="http://antiochblog.jp/praise/1706young/" target="_blank">J.GOSPELコンテスト U21・KIDS ファイナル in 白馬<br>出場決定喜びの一週間</a></p>
 </div>
 
-<!--<div class="column-inner">
+<div class="column-inner">
 <span class="label-1">ブログ</span>
 <span>NEW</span>
 <a href="http://antiochblog.jp/info/jerusalem2017/0615hakone/" target="_blank"><img src="/2016/images/top/1706jerumisato.jpg"></a>
 <p class="block-date">2017年6月15日</p>
 <p class="block-title"><a href="http://antiochblog.jp/info/jerusalem2017/0615hakone/" target="_blank">エルサレム<br>箱根のホテルでロビーコンサート</a></p>
-</div>-->
+</div>
 
 <div class="column-inner">
 <span class="label-1">ブログ</span>
