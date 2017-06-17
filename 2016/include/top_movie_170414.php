401628d3b472b91f42a1912aae3cd3c10dee03d1
timo77 <timo77@antioch.jp>
2017-06-17 18:00:44 +0900

    ゴミファイル削除

--- 2016/include/top_movie_170414.php
+++
@@ -1,5 +0,0 @@
-            <!-- movie -->
-            <div class="bxslider2">
-              <div><img src="/2016/images/top/slide/1612ad_jerusalem.jpg" /></div>
-            </div>
-            <!-- End movie -->
