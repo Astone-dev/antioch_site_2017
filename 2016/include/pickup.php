44dcc04d3e34439cb35e09163984fb03f2a41459
Samuel N Kasahara <samuel.livebyfaith@gmail.com>
2017-06-29 00:35:55 +0900

    PICKUP変更

--- 2016/include/pickup.php
+++ 2016/include/pickup.php
@@ -41,14 +41,6 @@
 <div class="column-inner">
 <span class="label-1">ブログ</span>
 <span>NEW</span>
-<a href="http://antiochblog.jp/info/1706kagoshima" target="_blank"><img src="/2016/images/top/1706familyhomekagoshima.jpg"></a>
-<p class="block-date">2017年6月28日〜29日</p>
-<p class="block-title"><a href="http://antiochblog.jp/info/1706kagoshima" target="_blank">ファミリーホームに向って<br>礎の石孤児院<br>鹿児島県庁にあいさつ手順をききにいく<br>(日程：2017年6月28日〜29日)</a></p>
-</div>
-
-<div class="column-inner">
-<span class="label-1">ブログ</span>
-<span>NEW</span>
 <a href="http://astone.tv/events/1706tech/" target="_blank"><img src="/2016/images/top/1706aiitresearch.jpg"></a>
 <p class="block-date">2017年6月28日〜30日</p>
 <p class="block-title"><a href="http://astone.tv/events/1706tech/" target="_blank">AI・IT最新技術展示会<br>リサーチ隊が行く</a></p>
@@ -57,10 +49,9 @@
 <div class="column-inner">
 <span class="label-1">ブログ</span>
 <span>NEW</span>
-<a href="http://antiochblog.jp/praise/nerima1706/" target="_blank"><img src="/2016/images/top/1706patolisnerima.jpg"></a>
-<p class="block-date">2017年6月28日</p>
-<p class="block-title"><a href="http://antiochblog.jp/praise/nerima1706/" target="_blank">PATOLIS<br>練馬区のデイサービス施設でコンサート<br>(日程：2017年6月28日)
-</a></p>
+<a href="http://antiochblog.jp/info/1706kagoshima" target="_blank"><img src="/2016/images/top/1706familyhomekagoshima.jpg"></a>
+<p class="block-date">2017年6月28日〜29日</p>
+<p class="block-title"><a href="http://antiochblog.jp/info/1706kagoshima" target="_blank">ファミリーホームに向って<br>礎の石孤児院<br>鹿児島県庁にあいさつ手順をききにいく<br>(日程：2017年6月28日〜29日)</a></p>
 </div>
 
 <div class="column-inner">
@@ -77,6 +68,13 @@
 <a href="http://astone.tv/events/songs1706/" target="_blank"><img src="/2016/images/top/1706sakushiyorokobizenkoku.jpg"></a>
 <p class="block-date">2017年6月26日〜7月2日</p>
 <p class="block-title"><a href="http://astone.tv/events/songs1706/" target="_blank">風のひびき5集<br>作詞・作曲者たちの一週間<br>さあ出すぞ！その前にちょっと一服<br>全国各教会</a></p>
+</div>
+
+<div class="column-inner">
+<span class="label-1">ブログ</span>
+<span>NEW</span>
+<a href="http://tokyo.antioch.jp/tcm/" target="_blank"><img src="/2016/images/top/1706tokari_digest_p.jpg"></a>
+<p class="block-title"><a href="http://tokyo.antioch.jp/tcm/" target="_blank">6月東京カリスマ聖会<br>ダイジェスト写真集<br>(きれいな写真版)<br>(聖会日程：2017年6月4日)</a></p>
 </div>
 
 <div class="column-inner">
@@ -120,16 +118,24 @@
 </div>
 
 <div class="column-inner">
+<span>NEW</span>
+<a href="http://kazenohibiki.blogspot.jp" target="_blank"><img src="/2016/images/top/1604kazenohibikiradio.jpg"></a>
+<p class="block-date">毎日19:00更新</p>
+<p class="block-title"><a href="http://kazenohibiki.blogspot.jp" target="_blank">インターネットラジオ<br>ゴスペル風のひびき<br>心に触れるゴスペルと楽しいおしゃべり 取っ替え引っ替え14組のパーソナリティー</a></p>
+</div>
+
+<div class="column-inner">
 <a href="http://antiochblog.jp/info/katararetekoto/" target="_blank"><img src="/2016/images/top/1704yorimichi27.jpg"></a>
 <p class="block-date">毎日7:00更新</p>
 <p class="block-title"><a href="http://antiochblog.jp/info/katararetekoto/" target="_blank">クリスチャンの寄り道 Vol.27<br>今回は「失敗した時に主に語られたこと」のお話です</a></p>
 </div>
 
 <div class="column-inner">
-<span>NEW</span>
-<a href="http://kazenohibiki.blogspot.jp" target="_blank"><img src="/2016/images/top/1604kazenohibikiradio.jpg"></a>
-<p class="block-date">毎日19:00更新</p>
-<p class="block-title"><a href="http://kazenohibiki.blogspot.jp" target="_blank">インターネットラジオ<br>ゴスペル風のひびき<br>心に触れるゴスペルと楽しいおしゃべり 取っ替え引っ替え14組のパーソナリティー</a></p>
+<span class="label-1">ブログ</span>
+<a href="http://antiochblog.jp/praise/nerima1706/" target="_blank"><img src="/2016/images/top/1706patolisnerima.jpg"></a>
+<p class="block-date">2017年6月28日</p>
+<p class="block-title"><a href="http://antiochblog.jp/praise/nerima1706/" target="_blank">PATOLIS<br>練馬区のデイサービス施設でコンサート<br>(日程：2017年6月28日)
+</a></p>
 </div>
 
 <div class="column-inner">
