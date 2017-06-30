e728f18a1b9b153b2af7d6529db7993adceed646
Samuel N Kasahara <samuel.livebyfaith@gmail.com>
2017-07-01 01:02:10 +0900

    PICKUP修正

--- 2016/include/pickup.php
+++ 2016/include/pickup.php
@@ -14,13 +14,13 @@
 <p class="block-title"><a href="https://www.youtube.com/watch?v=nIJ1EyOVzTo" target="_blank" onclick="javascript:pageTracker._trackPageview('J.GOSPELチャリティーコンサート生中継');">吉祥寺J.GOSPEL FES生中継<br> (日程：2017年5月20日 11:00〜)</a></p>
 </div>-->
 
-<!--<div class="column-inner">
+<div class="column-inner">
 <span class="label-1">ブログ</span>
 <span>NEW</span>
-<a href="http://antiochblog.jp/praise/1707charisma/" target="_blank"><img src="/2016/images/top/1706tokari.jpg"></a>
+<a href="http://antiochblog.jp/praise/1707charisma/" target="_blank"><img src="/2016/images/top/1707tokari.jpg"></a>
 <p class="block-date">2017年7月1日〜2日</p>
-<p class="block-title"><a href="http://antiochblog.jp/praise/1707charisma/" target="_blank">7月 東京カリスマ聖会</a></p>
-</div>-->
+<p class="block-title"><a href="http://antiochblog.jp/praise/1707charisma/" target="_blank">梅雨空を吹きとばせ<br>東京カリスマ聖会<br>スタッフ大忙し<br>(聖会日程：2017年7月2日)</a></p>
+</div>
 
 <div class="column-inner">
 <span class="label-1">ブログ</span>
@@ -46,7 +46,7 @@
 <p class="block-title"><a href="http://astone.tv/events/1706tech/" target="_blank">今日もまだ続く！<br>AI・IT最新技術展示会<br>リサーチ隊が行く</a></p>
 </div>
 
-<!--<div class="column-inner">
+<div class="column-inner">
 <span class="label-1">ブログ</span>
 <span>NEW</span>
 <a href="http://antiochblog.jp/info/jerusalem2017/0701kawaguchi/" target="_blank"><img src="/2016/images/top/1706jerumisato.jpg"></a>
@@ -65,10 +65,10 @@
 <div class="column-inner">
 <span class="label-1">ブログ</span>
 <span>NEW</span>
-<a href="http://antiochblog.jp/praise/1707freedom/" target="_blank"><img src="/2016/images/top/1706aiitresearch.jpg"></a>
+<a href="http://antiochblog.jp/praise/1707freedom/" target="_blank"><img src="/2016/images/top/1707freedomshisetsu.jpg"></a>
 <p class="block-date">2017年7月1日</p>
 <p class="block-title"><a href="http://antiochblog.jp/praise/1707freedom/" target="_blank">FREEDOM<br>施設でゴスペルコンサート</a></p>
-</div>-->
+</div>
 
 <div class="column-inner">
 <span class="label-1">ブログ</span>
