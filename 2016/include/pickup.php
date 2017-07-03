64a401ccc82748e09a480bc5efe93b77d9c0b861
Samuel N Kasahara <samuel.livebyfaith@gmail.com>
2017-07-03 10:57:43 +0900

    PICKUP修正

--- 2016/include/pickup.php
+++ 2016/include/pickup.php
@@ -43,7 +43,7 @@
 <span>NEW</span>
 <a href="http://astone.tv/events/sanbi1707/" target="_blank"><img src="/2016/images/top/1707jgosfinalteam.jpg"></a>
 <p class="block-date">2017年7月2日〜9日</p>
-<p class="block-title"><a href="http://astone.tv/events/sanbi1707/" target="_blank">賛美グループ出場順決まる<br>いよいよ大会直前の緊張の一週間</a></p>
+<p class="block-title"><a href="http://astone.tv/events/sanbi1707/" target="_blank">J.GOSPELコンテストファイナルin白馬<br>賛美グループ出場順決まる<br>いよいよ大会直前の緊張の一週間<br>(大会日程：2017年7月26日、27日)</a></p>
 </div>
 
 <div class="column-inner">
@@ -52,13 +52,6 @@
 <a href="http://antiochblog.jp/praise/1707rainy/" target="_blank"><img src="/2016/images/top/1707tsuyuakezenkoku.jpg"></a>
 <p class="block-date">2017年7月3日〜9日</p>
 <p class="block-title"><a href="http://antiochblog.jp/praise/1707rainy/" target="_blank">梅雨明け？の一週間<br>全国各教会<br></a></p>
-</div>
-
-<div class="column-inner">
-<span class="label-1">ブログ</span>
-<span>NEW</span>
-<a href="http://tokyo.antioch.jp/tcm/" target="_blank"><img src="/2016/images/top/1706tokari_digest_p.jpg"></a>
-<p class="block-title"><a href="http://tokyo.antioch.jp/tcm/" target="_blank">6月東京カリスマ聖会<br>ダイジェスト写真集<br>(きれいな写真版)<br>(聖会日程：2017年6月4日)</a></p>
 </div>
 
 <div class="column-inner">
@@ -126,6 +119,12 @@
 <a href="http://astone.tv/events/bosyuu1706/" target="_blank"><img src="/2016/images/top/1706sakushiyorokobiantioch.jpg"></a>
 <p class="block-date">2017年6月26日〜7月2日</p>
 <p class="block-title"><a href="http://astone.tv/events/bosyuu1706/" target="_blank">風のひびき5集<br>作詞・作曲者たちの一週間<br>さあ出すぞ！その前にちょっと一服<br>アンテオケ教会</a></p>
+</div>
+
+<div class="column-inner">
+<span class="label-1">ブログ</span>
+<a href="http://tokyo.antioch.jp/tcm/" target="_blank"><img src="/2016/images/top/1706tokari_digest_p.jpg"></a>
+<p class="block-title"><a href="http://tokyo.antioch.jp/tcm/" target="_blank">6月東京カリスマ聖会<br>ダイジェスト写真集<br>(きれいな写真版)<br>(聖会日程：2017年6月4日)</a></p>
 </div>
 
 <div class="column-inner">
