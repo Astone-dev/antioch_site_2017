53c972fd08721bc8b552634838cd5399f555347e
fujita-PC\fujita <johnfuji4845@gmail.com>
2017-07-05 22:31:47 +0900

    pick up更新(7/6-7分)

--- 2016/include/pickup.php
+++ 2016/include/pickup.php
@@ -22,6 +22,14 @@
 <p class="block-title"><a href="http://antiochblog.jp/praise/chubu1707/" target="_blank">初夏の緑の香りの中を<br>中部宣教の旅<br>(聖会日程：2017年7月3日〜6日)</a></p>
 </div>
 
+<!--<div class="column-inner">
+<span class="label-1">ブログ</span>
+<span>NEW</span>
+<a href="http://antiochblog.jp/praise/kawasaki1707/" target="_blank"><img src="/2016/images/top/1704chubuseikai.jpg"></a>
+<p class="block-date">2017年7月7日</p>
+<p class="block-title"><a href="http://antiochblog.jp/praise/kawasaki1707/" target="_blank">川崎聖会</a></p>
+</div>-->
+
 <div class="column-inner">
 <span class="label-1">ブログ</span>
 <span>NEW</span>
@@ -45,6 +53,14 @@
 <p class="block-date">2017年7月2日〜9日</p>
 <p class="block-title"><a href="http://astone.tv/events/sanbi1707/" target="_blank">J.GOSPELコンテストファイナルin白馬<br>賛美グループ出場順決まる<br>いよいよ大会直前の緊張の一週間<br>(大会日程：2017年7月26日、27日)</a></p>
 </div>
+
+<!--<div class="column-inner">
+<span class="label-1">ブログ</span>
+<span>NEW</span>
+<a href="http://antiochblog.jp/praise/freedom1707/" target="_blank"><img src="/2016/images/top/1707freedomshisetsu.jpg"></a>
+<p class="block-date">2017年7月6日</p>
+<p class="block-title"><a href="http://antiochblog.jp/praise/freedom1707/" target="_blank">FREEDOM<br>施設でゴスペルコンサート<br></a></p>
+</div>-->
 
 <div class="column-inner">
 <span class="label-1">ブログ</span>
