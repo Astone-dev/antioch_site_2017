fb6aec56bbf55a171f1a25f55a5c5b0d47f9c195
Samuel N Kasahara <samuel.livebyfaith@gmail.com>
2017-07-06 02:01:21 +0900

    PICKUP修正

--- 2016/include/pickup.php
+++ 2016/include/pickup.php
@@ -14,14 +14,6 @@
 <p class="block-title"><a href="https://www.youtube.com/watch?v=nIJ1EyOVzTo" target="_blank" onclick="javascript:pageTracker._trackPageview('J.GOSPELチャリティーコンサート生中継');">吉祥寺J.GOSPEL FES生中継<br> (日程：2017年5月20日 11:00〜)</a></p>
 </div>-->
 
-<div class="column-inner">
-<span class="label-1">ブログ</span>
-<span>NEW</span>
-<a href="http://antiochblog.jp/praise/chubu1707/" target="_blank"><img src="/2016/images/top/1704chubuseikai.jpg"></a>
-<p class="block-date">2017年7月1日〜6日</p>
-<p class="block-title"><a href="http://antiochblog.jp/praise/chubu1707/" target="_blank">初夏の緑の香りの中を<br>中部宣教の旅<br>(聖会日程：2017年7月3日〜6日)</a></p>
-</div>
-
 <!--<div class="column-inner">
 <span class="label-1">ブログ</span>
 <span>NEW</span>
@@ -54,13 +46,13 @@
 <p class="block-title"><a href="http://astone.tv/events/sanbi1707/" target="_blank">J.GOSPELコンテストファイナルin白馬<br>賛美グループ出場順決まる<br>いよいよ大会直前の緊張の一週間<br>(大会日程：2017年7月26日、27日)</a></p>
 </div>
 
-<!--<div class="column-inner">
+<div class="column-inner">
 <span class="label-1">ブログ</span>
 <span>NEW</span>
 <a href="http://antiochblog.jp/praise/freedom1707/" target="_blank"><img src="/2016/images/top/1707freedomshisetsu.jpg"></a>
 <p class="block-date">2017年7月6日</p>
 <p class="block-title"><a href="http://antiochblog.jp/praise/freedom1707/" target="_blank">FREEDOM<br>施設でゴスペルコンサート<br></a></p>
-</div>-->
+</div>
 
 <div class="column-inner">
 <span class="label-1">ブログ</span>
@@ -115,6 +107,13 @@
 <a href="http://kazenohibiki.blogspot.jp" target="_blank"><img src="/2016/images/top/1604kazenohibikiradio.jpg"></a>
 <p class="block-date">毎日19:00更新</p>
 <p class="block-title"><a href="http://kazenohibiki.blogspot.jp" target="_blank">インターネットラジオ<br>ゴスペル風のひびき<br>心に触れるゴスペルと楽しいおしゃべり 取っ替え引っ替え14組のパーソナリティー</a></p>
+</div>
+
+<div class="column-inner">
+<span class="label-1">ブログ</span>
+<a href="http://antiochblog.jp/praise/chubu1707/" target="_blank"><img src="/2016/images/top/1704chubuseikai.jpg"></a>
+<p class="block-date">2017年7月1日〜6日</p>
+<p class="block-title"><a href="http://antiochblog.jp/praise/chubu1707/" target="_blank">初夏の緑の香りの中を<br>中部宣教の旅<br>(聖会日程：2017年7月3日〜6日)</a></p>
 </div>
 
 <div class="column-inner">
