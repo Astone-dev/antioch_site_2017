d6570b39b1770850c52b6d33f18eb5f914ac80f4
Samuel N Kasahara <samuel.livebyfaith@gmail.com>
2017-07-07 01:28:00 +0900

    PICKUP修正

--- 2016/include/pickup.php
+++ 2016/include/pickup.php
@@ -65,14 +65,6 @@
 <div class="column-inner">
 <span class="label-1">ブログ</span>
 <span>NEW</span>
-<a href="http://antiochblog.jp/praise/freedom1707/" target="_blank"><img src="/2016/images/top/1707freedomshisetsu.jpg"></a>
-<p class="block-date">2017年7月6日</p>
-<p class="block-title"><a href="http://antiochblog.jp/praise/freedom1707/" target="_blank">FREEDOM<br>施設でゴスペルコンサート<br></a></p>
-</div>
-
-<div class="column-inner">
-<span class="label-1">ブログ</span>
-<span>NEW</span>
 <a href="http://antiochblog.jp/praise/1707rainy/" target="_blank"><img src="/2016/images/top/1707tsuyuakezenkoku.jpg"></a>
 <p class="block-date">2017年7月3日〜9日</p>
 <p class="block-title"><a href="http://antiochblog.jp/praise/1707rainy/" target="_blank">梅雨明け？の一週間<br>全国各教会<br></a></p>
@@ -123,6 +115,13 @@
 <a href="http://kazenohibiki.blogspot.jp" target="_blank"><img src="/2016/images/top/1604kazenohibikiradio.jpg"></a>
 <p class="block-date">毎日19:00更新</p>
 <p class="block-title"><a href="http://kazenohibiki.blogspot.jp" target="_blank">インターネットラジオ<br>ゴスペル風のひびき<br>心に触れるゴスペルと楽しいおしゃべり 取っ替え引っ替え14組のパーソナリティー</a></p>
+</div>
+
+<div class="column-inner">
+<span class="label-1">ブログ</span>
+<a href="http://antiochblog.jp/praise/freedom1707/" target="_blank"><img src="/2016/images/top/1707freedomshisetsu.jpg"></a>
+<p class="block-date">2017年7月6日</p>
+<p class="block-title"><a href="http://antiochblog.jp/praise/freedom1707/" target="_blank">FREEDOM<br>施設でゴスペルコンサート<br></a></p>
 </div>
 
 <div class="column-inner">
