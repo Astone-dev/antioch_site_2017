aa6d1c93a710090541f846fef54a4b38784b1900
Samuel N Kasahara <samuel.livebyfaith@gmail.com>
2017-07-08 01:31:17 +0900

    PICKUP修正

--- 2016/include/pickup.php
+++ 2016/include/pickup.php
@@ -33,22 +33,6 @@
 <div class="column-inner">
 <span class="label-1">ブログ</span>
 <span>NEW</span>
-<a href="http://astone.tv/events/2017healing/0708healing/" target="_blank"><img src="/2016/images/top/1706healing.jpg"></a>
-<p class="block-date">2017年7月8日</p>
-<p class="block-title"><a href="http://astone.tv/events/2017healing/0708healing/" target="_blank">7月いやしの集いの一日</a></p>
-</div>
-
-<div class="column-inner">
-<span class="label-2">映像</span>
-<span>NEW</span>
-<a href="https://www.youtube.com/channel/UCOV9zn7vX4O98uRzWlODMew/live" target="_blank" onclick="javascript:pageTracker._trackPageview('いやしの集い-生中継'); "><img src="/2016/images/top/1610hanbokushilive.png"></a>
-<p class="block-date">2017年7月8日</p>
-<p class="block-title"><a href="https://www.youtube.com/channel/UCOV9zn7vX4O98uRzWlODMew/live" target="_blank" onclick="javascript:pageTracker._trackPageview('いやしの集い-生中継');">いやしの集い 生中継<br> (日程：2017年7月8日 18:30スタート)</a></p>
-</div>
-
-<div class="column-inner">
-<span class="label-1">ブログ</span>
-<span>NEW</span>
 <a href="http://astone.tv/events/1707rainy" target="_blank"><img src="/2016/images/top/1707tsuyuakeantioch.jpg"></a>
 <p class="block-date">2017年7月3日〜9日</p>
 <p class="block-title"><a href="http://astone.tv/events/1707rainy" target="_blank">梅雨明け？の一週間<br>東京アンテオケ教会</a></p>
@@ -60,6 +44,22 @@
 <a href="http://antiochblog.jp/info/1707concert/" target="_blank"><img src="/2016/images/top/1707okefujin.jpg"></a>
 <p class="block-date">2017年7月7日-8日</p>
 <p class="block-title"><a href="http://antiochblog.jp/info/1707concert/" target="_blank">婦人会とオーケストラ<br>道の駅でコンサート<br>(日程：2017年7月8日)</a></p>
+</div>
+
+<div class="column-inner">
+<span class="label-1">ブログ</span>
+<span>NEW</span>
+<a href="http://astone.tv/events/2017healing/0708healing/" target="_blank"><img src="/2016/images/top/1706healing.jpg"></a>
+<p class="block-date">2017年7月8日</p>
+<p class="block-title"><a href="http://astone.tv/events/2017healing/0708healing/" target="_blank">7月いやしの集いの一日</a></p>
+</div>
+
+<div class="column-inner">
+<span class="label-2">映像</span>
+<span>NEW</span>
+<a href="https://www.youtube.com/channel/UCOV9zn7vX4O98uRzWlODMew/live" target="_blank" onclick="javascript:pageTracker._trackPageview('いやしの集い-生中継'); "><img src="/2016/images/top/1610hanbokushilive.png"></a>
+<p class="block-date">2017年7月8日</p>
+<p class="block-title"><a href="https://www.youtube.com/channel/UCOV9zn7vX4O98uRzWlODMew/live" target="_blank" onclick="javascript:pageTracker._trackPageview('いやしの集い-生中継');">いやしの集い 生中継<br> (日程：2017年7月8日 18:30スタート)</a></p>
 </div>
 
 <div class="column-inner">
