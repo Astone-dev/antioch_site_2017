44b34f0bdb2b4455defb57abeeaf612f9802bd03
fujita-PC\fujita <johnfuji4845@gmail.com>
2017-08-01 22:33:40 +0900

    pick up更新(8/2分)

--- 2016/include/pickup.php
+++ 2016/include/pickup.php
@@ -55,6 +55,14 @@
 <p class="block-title"><a href="http://antiochblog.jp/praise/yorokobiy1707/" target="_blank">J.GOSPELコンテスト U21・KIDSファイナル in 白馬<br>喜びと悲しみの一週間</a></p>
 </div>
 
+<!--<div class="column-inner">
+<span class="label-1">ブログ</span>
+<span>NEW</span>
+<a href="http://antiochblog.jp/info/jerusalem2017/1708kawaguchi/" target="_blank"><img src="/2016/images/top/1705jerumisato.jpg"></a>
+<p class="block-date">2017年8月2日</p>
+<p class="block-title"><a href="http://antiochblog.jp/info/jerusalem2017/1708kawaguchi/" target="_blank">エルサレム<br>川口市のリハビリセンターでゴスペル</a></p>
+</div>-->
+
 <div class="column-inner">
 <span class="label-1">ブログ</span>
 <span>NEW</span>
