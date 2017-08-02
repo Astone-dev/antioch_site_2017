868d98f15864b36b87dc559eeb4ceb61b4429576
Samuel N Kasahara <samuel.livebyfaith@gmail.com>
2017-08-02 09:14:16 +0900

    PICKUP修正

--- 2016/include/pickup.php
+++ 2016/include/pickup.php
@@ -55,20 +55,12 @@
 <p class="block-title"><a href="http://antiochblog.jp/praise/yorokobiy1707/" target="_blank">J.GOSPELコンテスト U21・KIDSファイナル in 白馬<br>喜びと悲しみの一週間</a></p>
 </div>
 
-<!--<div class="column-inner">
+<div class="column-inner">
 <span class="label-1">ブログ</span>
 <span>NEW</span>
 <a href="http://antiochblog.jp/info/jerusalem2017/1708kawaguchi/" target="_blank"><img src="/2016/images/top/1705jerumisato.jpg"></a>
 <p class="block-date">2017年8月2日</p>
 <p class="block-title"><a href="http://antiochblog.jp/info/jerusalem2017/1708kawaguchi/" target="_blank">エルサレム<br>川口市のリハビリセンターでゴスペル</a></p>
-</div>-->
-
-<div class="column-inner">
-<span class="label-1">ブログ</span>
-<span>NEW</span>
-<a href="http://astone.tv/events/euqaristias/20170731shinjuku/" target="_blank"><img src="/2016/images/top/1708eukarishinjyuku.jpg"></a>
-<p class="block-date">2017年7月31日</p>
-<p class="block-title"><a href="http://astone.tv/events/euqaristias/20170731shinjuku/" target="_blank">エウカリスティアス<br>新宿の福祉施設でゴスペル</a></p>
 </div>
 
 <div class="column-inner">
@@ -137,6 +129,13 @@
 <a href="http://kazenohibiki.blogspot.jp" target="_blank"><img src="/2016/images/top/1604kazenohibikiradio.jpg"></a>
 <p class="block-date">毎日19:00更新</p>
 <p class="block-title"><a href="http://kazenohibiki.blogspot.jp" target="_blank">インターネットラジオ<br>ゴスペル風のひびき<br>心に触れるゴスペルと楽しいおしゃべり 取っ替え引っ替え14組のパーソナリティー</a></p>
+</div>
+
+<div class="column-inner">
+<span class="label-1">ブログ</span>
+<a href="http://astone.tv/events/euqaristias/20170731shinjuku/" target="_blank"><img src="/2016/images/top/1708eukarishinjyuku.jpg"></a>
+<p class="block-date">2017年7月31日</p>
+<p class="block-title"><a href="http://astone.tv/events/euqaristias/20170731shinjuku/" target="_blank">エウカリスティアス<br>新宿の福祉施設でゴスペル</a></p>
 </div>
 
 <div class="column-inner">
