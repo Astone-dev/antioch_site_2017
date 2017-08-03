7b5c022e8c7a807eb9f6be95afed1d7693d91e80
Samuel N Kasahara <samuel.livebyfaith@gmail.com>
2017-08-03 11:39:57 +0900

    PICKUP修正

--- 2016/include/pickup.php
+++ 2016/include/pickup.php
@@ -26,14 +26,6 @@
 <div class="column-inner">
 <span class="label-1">ブログ</span>
 <span>NEW</span>
-<a href="http://antiochblog.jp/praise/kansai1708/" target="_blank"><img src="/2016/images/top/1708kansaiseikai.jpg"></a>
-<p class="block-date">2017年7月30日～8月3日</p>
-<p class="block-title"><a href="http://antiochblog.jp/praise/kansai1708/" target="_blank">夏真っ盛りの関西を行く<br>関西宣教の旅<br>(聖会日程：2017年7月31日〜8月2日)</a></p>
-</div>
-
-<div class="column-inner">
-<span class="label-1">ブログ</span>
-<span>NEW</span>
 <a href="http://antiochblog.jp/praise/1708antioch/" target="_blank"><img src="/2016/images/top/1707summer.jpg"></a>
 <p class="block-date">2017年7月31日～8月6日</p>
 <p class="block-title"><a href="http://antiochblog.jp/praise/1708antioch/" target="_blank">猛烈な夏<br>アンテオケ教会の一週間</a></p>
@@ -53,14 +45,6 @@
 <a href="http://antiochblog.jp/praise/yorokobiy1707/" target="_blank"><img src="/2016/images/top/1707contestu21kidsyorokobi.jpg"></a>
 <p class="block-date">2017年7月28日～</p>
 <p class="block-title"><a href="http://antiochblog.jp/praise/yorokobiy1707/" target="_blank">J.GOSPELコンテスト U21・KIDSファイナル in 白馬<br>喜びと悲しみの一週間</a></p>
-</div>
-
-<div class="column-inner">
-<span class="label-1">ブログ</span>
-<span>NEW</span>
-<a href="http://antiochblog.jp/info/jerusalem2017/1708kawaguchi/" target="_blank"><img src="/2016/images/top/1705jerumisato.jpg"></a>
-<p class="block-date">2017年8月2日</p>
-<p class="block-title"><a href="http://antiochblog.jp/info/jerusalem2017/1708kawaguchi/" target="_blank">エルサレム<br>川口市のリハビリセンターでゴスペル</a></p>
 </div>
 
 <div class="column-inner">
@@ -129,6 +113,20 @@
 <a href="http://kazenohibiki.blogspot.jp" target="_blank"><img src="/2016/images/top/1604kazenohibikiradio.jpg"></a>
 <p class="block-date">毎日19:00更新</p>
 <p class="block-title"><a href="http://kazenohibiki.blogspot.jp" target="_blank">インターネットラジオ<br>ゴスペル風のひびき<br>心に触れるゴスペルと楽しいおしゃべり 取っ替え引っ替え14組のパーソナリティー</a></p>
+</div>
+
+<div class="column-inner">
+<span class="label-1">ブログ</span>
+<a href="http://antiochblog.jp/praise/kansai1708/" target="_blank"><img src="/2016/images/top/1708kansaiseikai.jpg"></a>
+<p class="block-date">2017年7月30日～8月3日</p>
+<p class="block-title"><a href="http://antiochblog.jp/praise/kansai1708/" target="_blank">夏真っ盛りの関西を行く<br>関西宣教の旅<br>(聖会日程：2017年7月31日〜8月2日)</a></p>
+</div>
+
+<div class="column-inner">
+<span class="label-1">ブログ</span>
+<a href="http://antiochblog.jp/info/jerusalem2017/1708kawaguchi/" target="_blank"><img src="/2016/images/top/1705jerumisato.jpg"></a>
+<p class="block-date">2017年8月2日</p>
+<p class="block-title"><a href="http://antiochblog.jp/info/jerusalem2017/1708kawaguchi/" target="_blank">エルサレム<br>川口市のリハビリセンターでゴスペル</a></p>
 </div>
 
 <div class="column-inner">
