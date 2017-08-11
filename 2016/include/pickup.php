4b7267c6a724f6d7798cfbe277498d83d05bb696
fujita-PC\fujita <johnfuji4845@gmail.com>
2017-08-11 16:31:27 +0900

    pick up更新(8/12分)

--- 2016/include/pickup.php
+++ 2016/include/pickup.php
@@ -23,6 +23,28 @@
 <p class="block-title"><a href="http://www.ustream.tv/channel/tokyoantiochtv" target="_blank" onclick="javascript:pageTracker._trackPageview('パトモスチーム生中継');">パトモスチーム生中継<br>15:30頃スタート<br>（現地時間9:30頃スタート)</a></p>
 </div>-->
 
+<!--<div class="column-inner">
+<span class="label-1">ブログ</span>
+<a href="http://astone.tv/events/1708toyohashifuji/" target="_blank"><img src="/2016/images/top/1708kyusyuseika.jpg"></a>
+<p class="block-date">2017年8月12日</p>
+<p class="block-title"><a href="http://astone.tv/events/1708toyohashifuji/" target="_blank">真夏の光の中<br>豊橋・富士宣教の旅<br>(聖会日程：2017年8月7日～8月9日)</a></p>
+</div>-->
+
+<!--<div class="column-inner">
+<span class="label-1">ブログ</span>
+<a href="http://astone.tv/events/1708kofu/" target="_blank"><img src="/2016/images/top/1708kyusyuseika.jpg"></a>
+<p class="block-date">2017年8月12日</p>
+<p class="block-title"><a href="http://astone.tv/events/1708kofu/" target="_blank">Gifts,キブツ、<br>Truevineとチャリティーコンサート</a></p>
+</div>-->
+
+<!--<div class="column-inner">
+<span class="label-1">ブログ</span>
+<a href="http://astone.tv/events/2017healing/0812healing/
+" target="_blank"><img src="/2016/images/top/1706iyashinotsudoi.jpg"></a>
+<p class="block-date">2017年8月12日</p>
+<p class="block-title"><a href="http://astone.tv/events/2017healing/0812healing/" target="_blank">8月 いやしの集い</a></p>
+</div>-->
+
 <div class="column-inner">
 <span class="label-1">ブログ</span>
 <span>NEW</span>
