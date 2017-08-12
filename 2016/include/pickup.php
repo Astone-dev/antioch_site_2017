de5af952b0616146bae638c9df64d99cfe2ec8bd
Samuel N Kasahara <samuel.livebyfaith@gmail.com>
2017-08-13 00:17:35 +0900

    PICKUP修正

--- 2016/include/pickup.php
+++ 2016/include/pickup.php
@@ -17,41 +17,9 @@
 <div class="column-inner">
 <span class="label-1">ブログ</span>
 <span>NEW</span>
-<a href="http://astone.tv/events/1708toyohashifuji/" target="_blank"><img src="/2016/images/top/1708toyohashifuji.jpg"></a>
-<p class="block-date">2017年8月12日</p>
-<p class="block-title"><a href="http://astone.tv/events/1708toyohashifuji/" target="_blank">夏のまぶしい光の中を<br>豊橋・富士宣教の旅<br>(聖会日程：2017年8月12日)</a></p>
-</div>
-
-<!--<div class="column-inner">
-<span class="label-1">ブログ</span>
-<span>NEW</span>
-<a href="http://antiochblog.jp/praise/1708hokuriku/" target="_blank"><img src="/2016/images/top/1708toyohashifuji.jpg"></a>
+<a href="http://antiochblog.jp/praise/1708hokuriku/" target="_blank"><img src="/2016/images/top/1708hokuriku.jpg"></a>
 <p class="block-date">2017年8月13日～8月17日</p>
-<p class="block-title"><a href="http://antiochblog.jp/praise/1708hokuriku/" target="_blank">夏のまぶしい光の中を<br>北陸宣教の旅<br>(聖会日程：2017年8月14日～8月16日)</a></p>
-</div>-->
-
-<div class="column-inner">
-<span class="label-1">ブログ</span>
-<span>NEW</span>
-<a href="http://astone.tv/events/1708kofu/" target="_blank"><img src="/2016/images/top/1708giftskofu.jpg"></a>
-<p class="block-date">2017年8月12日</p>
-<p class="block-title"><a href="http://astone.tv/events/1708kofu/" target="_blank">Gifts、キブツ<br>TrueVineと甲府でチャリティーコンサート<br>(日程：2017年8月12日)</a></p>
-</div>
-
-<div class="column-inner">
-<span class="label-1">ブログ</span>
-<span>NEW</span>
-<a href="http://astone.tv/events/2017healing/0812healing/" target="_blank"><img src="/2016/images/top/1706iyashinotsudoi.jpg"></a>
-<p class="block-date">2017年8月12日</p>
-<p class="block-title"><a href="http://astone.tv/events/2017healing/0812healing/" target="_blank">8月いやしの集いの一日<br>(日程：2017年8月12日)</a></p>
-</div>
-
-<div class="column-inner">
-<span class="label-2">映像</span>
-<span>NEW</span>
-<a href="https://www.youtube.com/channel/UCOV9zn7vX4O98uRzWlODMew/live" target="_blank" onclick="javascript:pageTracker._trackPageview('いやしの集い-生中継'); "><img src="/2016/images/top/1610hanbokushilive.png"></a>
-<p class="block-date">2017年8月12日</p>
-<p class="block-title"><a href="https://www.youtube.com/channel/UCOV9zn7vX4O98uRzWlODMew/live" target="_blank" onclick="javascript:pageTracker._trackPageview('いやしの集い-生中継');">いやしの集い 生中継<br> (日程：2017年8月12日 18:30スタート)</a></p>
+<p class="block-title"><a href="http://antiochblog.jp/praise/1708hokuriku/" target="_blank">真夏の光の中を<br>北陸宣教の旅<br>(聖会日程：2017年8月14日～8月16日)</a></p>
 </div>
 
 <!--<div class="column-inner">
@@ -70,16 +38,15 @@
 <p class="block-title"><a href="http://antiochblog.jp/mission/event1708b/" target="_blank">アンテオケ教会<br>それぞれの夏休み一週間</a></p>
 </div>
 
-<!--
 <div class="column-inner">
 <span class="label-1">ブログ</span>
+<span>NEW</span>
 <a href="http://antiochblog.jp/info/jerusalem2017/0813other/" target="_blank"><img src="/2016/images/top/1705jerumisato.jpg"></a>
 <p class="block-date">2017年8月13日</p>
 <p class="block-title"><a href="http://antiochblog.jp/info/jerusalem2017/0813other/" target="_blank">エルサレム<br>他教会で奉仕</a></p>
 </div>
 
-
-<div class="column-inner">
+<!--<div class="column-inner">
 <span class="label-1">ブログ</span>
 <span>NEW</span>
 <a href="http://astone.tv/events/kokogos1708/" target="_blank"><img src="/2016/images/top/1708kokorogospelnimukatte.jpg"></a>
@@ -87,21 +54,21 @@
 <p class="block-title"><a href="http://astone.tv/events/kokogos1708/" target="_blank">Euqaristias<br>練馬デイサービスでコンサート</a></p>
 </div>-->
 
-<!--<div class="column-inner">
+<div class="column-inner">
 <span class="label-1">ブログ</span>
 <span>NEW</span>
 <a href="http://astone.tv/events/2017byhisspirit/0813concert/" target="_blank"><img src="/2016/images/top/1706byhis.jpg"></a>
 <p class="block-date">2017年8月13日</p>
 <p class="block-title"><a href="http://astone.tv/events/2017byhisspirit/0813concert/" target="_blank">By His Spirit<br>世田谷の高齢者施設でゴスペルコンサート</a></p>
-</div>-->
+</div>
 
-<!--<div class="column-inner">
+<div class="column-inner">
 <span class="label-1">ブログ</span>
 <span>NEW</span>
 <a href="http://astone.tv/events/1708gracious/" target="_blank"><img src="/2016/images/top/1704gracious.jpg"></a>
 <p class="block-date">2017年8月13日</p>
 <p class="block-title"><a href="http://astone.tv/events/1708gracious/" target="_blank">GRACIOUS  DOORS<br>練馬区のデイサービスでゴスペルコンサート</a></p>
-</div>-->
+</div>
 
 <div class="column-inner">
 <span class="label-1">ブログ</span>
@@ -123,28 +90,6 @@
 <a href="http://gospelconcert.jp/cm.html" target="_blank"><img src="/2016/images/top/1707kokorocm.jpg"></a>
 <p class="block-title"><a href="http://gospelconcert.jp/cm.html" target="_blank">こころごすぺるCM<br>(日程：2017年8月11日 19:00開演 18:30開場 会場:なかのZEROホール)</a></p>
 </div>-->
-
-<div class="column-inner">
-<span class="label-2">映像</span>
-<span>NEW</span>
-<a href="http://tv.antioch.jp/digest/201707-hakuba/" target="_blank"><img src="/2016/images/top/1707hakuba.jpg"></a>
-<p class="block-title"><a href="http://tv.antioch.jp/digest/201707-hakuba/" target="_blank">白馬キャンプ<br>ダイジェスト映像<br>(チーム日程：2017年7月25日〜28日)</a></p>
-</div>
-
-<div class="column-inner">
-<span class="label-2">映像</span>
-<span>NEW</span>
-<a href="http://tv.antioch.jp/digest/201707patmos/" target="_blank"><img src="/2016/images/top/1707patmosdigest_m.jpg"></a>
-<p class="block-title"><a href="http://tv.antioch.jp/digest/201707patmos/" target="_blank">パトモスチーム<br>ダイジェスト映像<br>(チーム日程：2017年7月11日〜20日)</a></p>
-</div>
-
-<div class="column-inner">
-<span class="label-2">映像</span>
-<span>NEW</span>
-<a href="http://blog.antioch.jp/1707patmos/category/dst/" target="_blank"><img src="/2016/images/top/1708patmosudigest_p.jpg"></a>
-<p class="block-title"><a href="http://blog.antioch.jp/1707patmos/category/dst/" target="_blank">パトモスチーム<br>ダイジェスト写真集<br>(短く全てを知りたい人のため)<br>(チーム日程：2017年7月11日〜20日)</a></p>
-</div>
-
 
 <!--<div class="column-inner">
 <span>NEW</span>
@@ -176,6 +121,45 @@
 <a href="http://kazenohibiki.blogspot.jp" target="_blank"><img src="/2016/images/top/1604kazenohibikiradio.jpg"></a>
 <p class="block-date">毎日19:00更新</p>
 <p class="block-title"><a href="http://kazenohibiki.blogspot.jp" target="_blank">インターネットラジオ<br>ゴスペル風のひびき<br>心に触れるゴスペルと楽しいおしゃべり 取っ替え引っ替え14組のパーソナリティー</a></p>
+</div>
+
+<div class="column-inner">
+<span class="label-1">ブログ</span>
+<a href="http://astone.tv/events/1708toyohashifuji/" target="_blank"><img src="/2016/images/top/1708toyohashifuji.jpg"></a>
+<p class="block-date">2017年8月12日</p>
+<p class="block-title"><a href="http://astone.tv/events/1708toyohashifuji/" target="_blank">夏のまぶしい光の中を<br>豊橋・富士宣教の旅<br>(聖会日程：2017年8月12日)</a></p>
+</div>
+
+<div class="column-inner">
+<span class="label-1">ブログ</span>
+<a href="http://astone.tv/events/1708kofu/" target="_blank"><img src="/2016/images/top/1708giftskofu.jpg"></a>
+<p class="block-date">2017年8月12日</p>
+<p class="block-title"><a href="http://astone.tv/events/1708kofu/" target="_blank">Gifts、キブツ<br>TrueVineと甲府でチャリティーコンサート<br>(日程：2017年8月12日)</a></p>
+</div>
+
+<div class="column-inner">
+<span class="label-1">ブログ</span>
+<a href="http://astone.tv/events/2017healing/0812healing/" target="_blank"><img src="/2016/images/top/1706iyashinotsudoi.jpg"></a>
+<p class="block-date">2017年8月12日</p>
+<p class="block-title"><a href="http://astone.tv/events/2017healing/0812healing/" target="_blank">8月いやしの集いの一日<br>(日程：2017年8月12日)</a></p>
+</div>
+
+<div class="column-inner">
+<span class="label-2">映像</span>
+<a href="http://tv.antioch.jp/digest/201707-hakuba/" target="_blank"><img src="/2016/images/top/1707hakuba.jpg"></a>
+<p class="block-title"><a href="http://tv.antioch.jp/digest/201707-hakuba/" target="_blank">白馬キャンプ<br>ダイジェスト映像<br>(チーム日程：2017年7月25日〜28日)</a></p>
+</div>
+
+<div class="column-inner">
+<span class="label-2">映像</span>
+<a href="http://tv.antioch.jp/digest/201707patmos/" target="_blank"><img src="/2016/images/top/1707patmosdigest_m.jpg"></a>
+<p class="block-title"><a href="http://tv.antioch.jp/digest/201707patmos/" target="_blank">パトモスチーム<br>ダイジェスト映像<br>(チーム日程：2017年7月11日〜20日)</a></p>
+</div>
+
+<div class="column-inner">
+<span class="label-2">映像</span>
+<a href="http://blog.antioch.jp/1707patmos/category/dst/" target="_blank"><img src="/2016/images/top/1708patmosudigest_p.jpg"></a>
+<p class="block-title"><a href="http://blog.antioch.jp/1707patmos/category/dst/" target="_blank">パトモスチーム<br>ダイジェスト写真集<br>(短く全てを知りたい人のため)<br>(チーム日程：2017年7月11日〜20日)</a></p>
 </div>
 
 <div class="column-inner">
