45d8da5f9d96d3c89094b511dae8af7b8cb0643b
fujita-PC\fujita <johnfuji4845@gmail.com>
2017-08-14 23:43:30 +0900

    pick up変更

--- 2016/include/pickup.php
+++ 2016/include/pickup.php
@@ -51,14 +51,6 @@
 <div class="column-inner">
 <span class="label-1">ブログ</span>
 <span>NEW</span>
-<a href="http://antiochblog.jp/mission/patolis1708/" target="_blank"><img src="/2016/images/top/1706patolisnerima.jpg"></a>
-<p class="block-date">8月13日</p>
-<p class="block-title"><a href="http://antiochblog.jp/mission/patolis1708/" target="_blank">PATOLIS<br>練馬デイサービスでコンサート</a></p>
-</div>
-
-<div class="column-inner">
-<span class="label-1">ブログ</span>
-<span>NEW</span>
 <a href="http://antiochblog.jp/praise/event1708/" target="_blank"><img src="/2016/images/top/1708summerzenkoku.jpg"></a>
 <p class="block-date">2017年8月7日～8月20日</p>
 <p class="block-title"><a href="http://antiochblog.jp/praise/event1708/" target="_blank">ウキウキの夏休み<br />
@@ -115,6 +107,13 @@
 <a href="http://kazenohibiki.blogspot.jp" target="_blank"><img src="/2016/images/top/1604kazenohibikiradio.jpg"></a>
 <p class="block-date">毎日19:00更新</p>
 <p class="block-title"><a href="http://kazenohibiki.blogspot.jp" target="_blank">インターネットラジオ<br>ゴスペル風のひびき<br>心に触れるゴスペルと楽しいおしゃべり 取っ替え引っ替え14組のパーソナリティー</a></p>
+</div>
+
+<div class="column-inner">
+<span class="label-1">ブログ</span>
+<a href="http://antiochblog.jp/mission/patolis1708/" target="_blank"><img src="/2016/images/top/1706patolisnerima.jpg"></a>
+<p class="block-date">8月13日</p>
+<p class="block-title"><a href="http://antiochblog.jp/mission/patolis1708/" target="_blank">PATOLIS<br>練馬デイサービスでコンサート</a></p>
 </div>
 
 <div class="column-inner">
