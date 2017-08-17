a03c23505e9a31d4cd11d7119d803675bba203a1
Samuel N Kasahara <samuel.livebyfaith@gmail.com>
2017-08-18 00:59:14 +0900

    PICKUP修正

--- 2016/include/pickup.php
+++ 2016/include/pickup.php
@@ -49,14 +49,6 @@
 <div class="column-inner">
 <span class="label-1">ブログ</span>
 <span>NEW</span>
-<a href="http://antiochblog.jp/praise/uranos2017/category/08kodaira/" target="_blank"><img src="/2016/images/top/1708uranos.jpg"></a>
-<p class="block-date">2017年8月17日</p>
-<p class="block-title"><a href="http://antiochblog.jp/praise/uranos2017/category/08kodaira/" target="_blank">ウラノス<br />小平市の国立病院</a></p>
-</div>
-
-<div class="column-inner">
-<span class="label-1">ブログ</span>
-<span>NEW</span>
 <a href="http://antiochblog.jp/praise/event1708/" target="_blank"><img src="/2016/images/top/1708summerzenkoku.jpg"></a>
 <p class="block-date">2017年8月7日～8月20日</p>
 <p class="block-title"><a href="http://antiochblog.jp/praise/event1708/" target="_blank">ウキウキの夏休み<br />
@@ -113,6 +105,13 @@
 <a href="http://kazenohibiki.blogspot.jp" target="_blank"><img src="/2016/images/top/1604kazenohibikiradio.jpg"></a>
 <p class="block-date">毎日19:00更新</p>
 <p class="block-title"><a href="http://kazenohibiki.blogspot.jp" target="_blank">インターネットラジオ<br>ゴスペル風のひびき<br>心に触れるゴスペルと楽しいおしゃべり 取っ替え引っ替え14組のパーソナリティー</a></p>
+</div>
+
+<div class="column-inner">
+<span class="label-1">ブログ</span>
+<a href="http://antiochblog.jp/praise/uranos2017/category/08kodaira/" target="_blank"><img src="/2016/images/top/1708uranos.jpg"></a>
+<p class="block-date">2017年8月17日</p>
+<p class="block-title"><a href="http://antiochblog.jp/praise/uranos2017/category/08kodaira/" target="_blank">ウラノス<br />小平市の国立病院</a></p>
 </div>
 
 <div class="column-inner">
