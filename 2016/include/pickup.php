68ef61ebca3dd3005005c496cff0364af2d70204
Samuel N Kasahara <samuel.livebyfaith@gmail.com>
2017-08-17 10:04:32 +0900

    PICKUP修正

--- 2016/include/pickup.php
+++ 2016/include/pickup.php
@@ -17,11 +17,11 @@
 <div class="column-inner">
 <span class="label-1">ブログ</span>
 <span>NEW</span>
-<a href="http://antiochblog.jp/praise/1708hokuriku/" target="_blank"><img src="/2016/images/top/1708hokuriku.jpg"></a>
-<p class="block-date">2017年8月13日～8月17日</p>
-<p class="block-title"><a href="http://antiochblog.jp/praise/1708hokuriku/" target="_blank">真夏の光の中を<br>北陸宣教の旅<br>(聖会日程：2017年8月14日～8月16日)</a></p>
+<a href="http://antiochblog.jp/mission/event1708b/" target="_blank"><img src="/2016/images/top/1708summerantioch.jpg"></a>
+<p class="block-date">2017年8月7日～8月20日</p>
+<p class="block-title"><a href="http://antiochblog.jp/mission/event1708b/" target="_blank">ワクワクの夏休み<br />
+アンテオケ教会一週間</a></p>
 </div>
-
 
 <div class="column-inner">
 <span class="label-1">ブログ</span>
@@ -42,19 +42,10 @@
 <div class="column-inner">
 <span class="label-1">ブログ</span>
 <span>NEW</span>
-<a href="http://antiochblog.jp/mission/event1708b/" target="_blank"><img src="/2016/images/top/1708summerantioch.jpg"></a>
-<p class="block-date">2017年8月7日～8月20日</p>
-<p class="block-title"><a href="http://antiochblog.jp/mission/event1708b/" target="_blank">ワクワクの夏休み<br />
-アンテオケ教会一週間</a></p>
-</div>
-
-<!--<div class="column-inner">
-<span class="label-1">ブログ</span>
-<span>NEW</span>
 <a href="http://antiochblog.jp/praise/uranos2017/category/08kodaira/" target="_blank"><img src="/2016/images/top/1708uranos.jpg"></a>
 <p class="block-date">2017年8月17日</p>
 <p class="block-title"><a href="http://antiochblog.jp/praise/uranos2017/category/08kodaira/" target="_blank">ウラノス<br />小平市の国立病院</a></p>
-</div>-->
+</div>
 
 <div class="column-inner">
 <span class="label-1">ブログ</span>
@@ -115,6 +106,13 @@
 <a href="http://kazenohibiki.blogspot.jp" target="_blank"><img src="/2016/images/top/1604kazenohibikiradio.jpg"></a>
 <p class="block-date">毎日19:00更新</p>
 <p class="block-title"><a href="http://kazenohibiki.blogspot.jp" target="_blank">インターネットラジオ<br>ゴスペル風のひびき<br>心に触れるゴスペルと楽しいおしゃべり 取っ替え引っ替え14組のパーソナリティー</a></p>
+</div>
+
+<div class="column-inner">
+<span class="label-1">ブログ</span>
+<a href="http://antiochblog.jp/praise/1708hokuriku/" target="_blank"><img src="/2016/images/top/1708hokuriku.jpg"></a>
+<p class="block-date">2017年8月13日～8月17日</p>
+<p class="block-title"><a href="http://antiochblog.jp/praise/1708hokuriku/" target="_blank">真夏の光の中を<br>北陸宣教の旅<br>(聖会日程：2017年8月14日～8月16日)</a></p>
 </div>
 
 <div class="column-inner">
