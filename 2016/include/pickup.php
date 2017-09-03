625c1c24ed710119709f21bba048fe9a74b1f872
Samuel N Kasahara <samuel.livebyfaith@gmail.com>
2017-09-04 01:00:30 +0900

    PICKUP修正

--- 2016/include/pickup.php
+++ 2016/include/pickup.php
@@ -17,6 +17,64 @@
 <div class="column-inner">
 <span class="label-1">ブログ</span>
 <span>NEW</span>
+<a href="http://blog.antioch.jp/ukraine1709slovakia/" target="_blank"><img src="/2016/images/top/1708ukraineteam.jpg"></a>
+<p class="block-date">日程：2017年9月4日〜9日</p>
+<p class="block-title"><a href="http://blog.antioch.jp/ukraine1709slovakia/" target="_blank">ウクライナ・スロバキアチーム<br>最新情報写真集<br>（チーム日程：2017年9月4日～9月9日）</a></p>
+</div>
+
+<div class="column-inner">
+<span class="label-1">ブログ</span>
+<span>NEW</span>
+<a href="http://antiochblog.jp/praise/new1709/" target="_blank"><img src="/2016/images/top/1709shingakkiantioch"></a>
+<p class="block-date">日程：2017年9月4日〜11日</p>
+<p class="block-title"><a href="http://antiochblog.jp/praise/new1709/" target="_blank">始まった新学期<br>東京アンテオケ教会の一週間</a></p>
+</div>
+
+<div class="column-inner">
+<span class="label-1">ブログ</span>
+<span>NEW</span>
+<a href="http://antiochblog.jp/praise/songs1709/" target="_blank"><img src="/2016/images/top/1709sakushiyorokobi.jpg"></a>
+<p class="block-date">日程：2017年9月4日〜11日</p>
+<p class="block-title"><a href="http://antiochblog.jp/praise/songs1709/" target="_blank">風のひびき5集<br>作詞・作曲 採用者喜びの一週間</a></p>
+</div>
+
+<div class="column-inner">
+<span class="label-1">ブログ</span>
+<span>NEW</span>
+<a href="http://astone.tv/events/faith1709/" target="_blank"><img src="/2016/images/top/1709shingakkizenkoku.jpg"></a>
+<p class="block-date">日程：2017年9月4日〜11日</p>
+<p class="block-title"><a href="http://astone.tv/events/faith1709/" target="_blank">始まった新学期<br>全国各教会の一週間</a></p>
+</div>
+
+<div class="column-inner">
+<span>NEW</span>
+<a href="http://antiochblog.jp/info/important/" target="_blank"><img src="/2016/images/top/1709yorimichi29.jpg"></a>
+<p class="block-date">毎日7:00更新</p>
+<p class="block-title"><a href="http://antiochblog.jp/info/important/" target="_blank">クリスチャンの寄り道 Vol.29<br>今回は「大事な事柄が分かった時」のお話です</a></p>
+</div>
+
+<div class="column-inner">
+<span>NEW</span>
+<a href="http://antiochblog.jp/info/standup/" target="_blank"><img src="/2016/images/top/1706yorimichi28.jpg"></a>
+<p class="block-date">毎日7:00更新</p>
+<p class="block-title"><a href="http://antiochblog.jp/info/standup/" target="_blank">クリスチャンの寄り道 Vol.28<br>今回は「倒れて立ち上がった経験」のお話です</a></p>
+</div>
+
+<div class="column-inner">
+<span>NEW</span>
+<a href="http://tokyo.antioch.jp/yorimichi/result-disappoint2.html" target="_blank"><img src="/2016/images/top/1707yorimichi27kekka.jpg"></a>
+<p class="block-title"><a href="http://tokyo.antioch.jp/yorimichi/result-disappoint2.html" target="_blank">ついに決定！<br>クリスチャンの寄り道 Vol.27<br>「失敗した時に主に語られたこと」のお話し人気ベスト10<br>ぜひお聴きください。</a></p>
+</div>
+
+<div class="column-inner">
+<span>NEW</span>
+<a href="http://kazenohibiki.blogspot.jp" target="_blank"><img src="/2016/images/top/1604kazenohibikiradio.jpg"></a>
+<p class="block-date">毎日19:00更新</p>
+<p class="block-title"><a href="http://kazenohibiki.blogspot.jp" target="_blank">インターネットラジオ<br>ゴスペル風のひびき<br>心に触れるゴスペルと楽しいおしゃべり 取っ替え引っ替え14組のパーソナリティー</a></p>
+</div>
+
+<div class="column-inner">
+<span class="label-1">ブログ</span>
 <a href="http://antiochblog.jp/info/1708event/" target="_blank"><img src="/2016/images/top/1709tochari.jpg"></a>
 <p class="block-date">日程：2017年9月2日〜9月3日</p>
 <p class="block-title"><a href="http://antiochblog.jp/info/1708event/" target="_blank">大忙し<br>東京カリスマ聖会の表と舞台裏<br>(日程：2017年9月3日)</a></p>
@@ -24,31 +82,13 @@
 
 <div class="column-inner">
 <span class="label-1">ブログ</span>
-<span>NEW</span>
 <a href="http://antiochblog.jp/praise/newsemester1708a/" target="_blank"><img src="/2016/images/top/1708mousugushingakiantioch.jpg"></a>
 <p class="block-date">日程：2017年8月28日〜9月3日</p>
 <p class="block-title"><a href="http://antiochblog.jp/praise/newsemester1708a/" target="_blank">もう少しで新学期<br>東京アンテオケ教会の一週間</a></p>
 </div>
 
-<!--<div class="column-inner">
-<span class="label-1">ブログ</span>
-<span>NEW</span>
-<a href="http://antiochblog.jp/praise/new1709/" target="_blank"><img src="/2016/images/top/1708mousugushingakiantioch.jpg"></a>
-<p class="block-date">日程：2017年9月4日〜11日</p>
-<p class="block-title"><a href="http://antiochblog.jp/praise/new1709/" target="_blank">始まった新学期<br>東京アンテオケ教会の一週間</a></p>
-</div>-->
-
-<!--<div class="column-inner">
-<span class="label-1">ブログ</span>
-<span>NEW</span>
-<a href="http://antiochblog.jp/praise/songs1709/" target="_blank"><img src="/2016/images/top/1708mousugushingakiantioch.jpg"></a>
-<p class="block-date">日程：2017年9月4日〜11日</p>
-<p class="block-title"><a href="http://antiochblog.jp/praise/songs1709/" target="_blank">風のひびき5集作詞・作曲<br>採用者喜びの一週間</a></p>
-</div>-->
-
 <div class="column-inner">
 <span class="label-1">ブログ</span>
-<span>NEW</span>
 <a href="http://astone.tv/events/selection1708/" target="_blank"><img src="/2016/images/top/1708shinsainweek.jpg"></a>
 <p class="block-date">日程：2017年8月28日〜9月3日</p>
 <p class="block-title"><a href="http://astone.tv/events/selection1708/" target="_blank">風のひびき5集 応募曲422曲<br>審査員激闘の一週間</a></p>
@@ -56,15 +96,6 @@
 
 <div class="column-inner">
 <span class="label-1">ブログ</span>
-<span>NEW</span>
-<a href="http://blog.antioch.jp/ukraine1709slovakia/" target="_blank"><img src="/2016/images/top/1708ukraineteam.jpg"></a>
-<p class="block-date">日程：2017年8月28日〜9月3日</p>
-<p class="block-title"><a href="http://blog.antioch.jp/ukraine1709slovakia/" target="_blank">ウクライナ・スロバキアチーム<br>直前情報写真集<br>（チーム日程：2017年9月4日～9月9日）</a></p>
-</div>
-
-<div class="column-inner">
-<span class="label-1">ブログ</span>
-<span>NEW</span>
 <a href="http://astone.tv/events/thanks1709/" target="_blank"><img src="/2016/images/top/1709gifts.jpg"></a>
 <p class="block-date">日程：2017年9月2日〜9月3日</p>
 <p class="block-title"><a href="http://astone.tv/events/thanks1709/" target="_blank">Gifts<br>秋の長野を行く 医療フォーラム</a></p>
@@ -88,23 +119,13 @@
 
 <div class="column-inner">
 <span class="label-1">ブログ</span>
-<span>NEW</span>
 <a href="http://astone.tv/events/newsemester1708/" target="_blank"><img src="/2016/images/top/1708mousugushingakizenkoku.jpg"></a>
 <p class="block-date">日程：2017年8月28日〜9月3日</p>
 <p class="block-title"><a href="http://astone.tv/events/newsemester1708/" target="_blank">もう少しで新学期<br>全国各教会の一週間</a></p>
 </div>
 
-<!--<div class="column-inner">
-<span class="label-1">ブログ</span>
-<span>NEW</span>
-<a href="http://astone.tv/events/faith1709/" target="_blank"><img src="/2016/images/top/1708mousugushingakiantioch.jpg"></a>
-<p class="block-date">日程：2017年9月4日〜11日</p>
-<p class="block-title"><a href="http://astone.tv/events/faith1709/" target="_blank">始まった新学期<br>全国各教会の一週間</a></p>
-</div>-->
-
 <div class="column-inner">
 <span class="label-2">映像</span>
-<span>NEW</span>
 <a href="http://tv.antioch.jp/digest/201708cambodia/" target="_blank"><img src="/2016/images/top/1708cambodiadigest_m.jpg"></a>
 <p class="block-date">日程：2017年8月21日〜24日</p>
 <p class="block-title"><a href="http://tv.antioch.jp/digest/201708cambodia/" target="_blank">カンボジアチーム<br>ダイジェスト映像<br>(再生時間 5:14)</a></p>
@@ -112,37 +133,9 @@
 
 <div class="column-inner">
 <span class="label-2">映像</span>
-<span>NEW</span>
 <a href="http://blog.antioch.jp/1708cambodia/category/1708dst/" target="_blank"><img src="/2016/images/top/1708cambodiadigest_p.jpg"></a>
 <p class="block-date">日程：2017年8月21日〜24日</p>
 <p class="block-title"><a href="http://blog.antioch.jp/1708cambodia/category/1708dst/" target="_blank">カンボジアチーム<br>ダイジェスト写真集<br>(短く全てを知りたい人のために)</a></p>
-</div>
-
-<div class="column-inner">
-<span>NEW</span>
-<a href="http://antiochblog.jp/info/standup/" target="_blank"><img src="/2016/images/top/1706yorimichi28.jpg"></a>
-<p class="block-date">毎日7:00更新</p>
-<p class="block-title"><a href="http://antiochblog.jp/info/standup/" target="_blank">クリスチャンの寄り道 Vol.28<br>今回は「倒れて立ち上がった経験」のお話です</a></p>
-</div>
-
-<!--<div class="column-inner">
-<span>NEW</span>
-<a href="http://antiochblog.jp/info/important/" target="_blank"><img src="/2016/images/top/1706yorimichi28.jpg"></a>
-<p class="block-date">毎日7:00更新</p>
-<p class="block-title"><a href="http://antiochblog.jp/info/important/" target="_blank">クリスチャンの寄り道 Vol.29<br>今回は「倒れて立ち上がった経験」のお話です</a></p>
-</div>-->
-
-<div class="column-inner">
-<span>NEW</span>
-<a href="http://tokyo.antioch.jp/yorimichi/result-disappoint2.html" target="_blank"><img src="/2016/images/top/1707yorimichi27kekka.jpg"></a>
-<p class="block-title"><a href="http://tokyo.antioch.jp/yorimichi/result-disappoint2.html" target="_blank">ついに決定！<br>クリスチャンの寄り道 Vol.27<br>「失敗した時に主に語られたこと」のお話し人気ベスト10<br>ぜひお聴きください。</a></p>
-</div>
-
-<div class="column-inner">
-<span>NEW</span>
-<a href="http://kazenohibiki.blogspot.jp" target="_blank"><img src="/2016/images/top/1604kazenohibikiradio.jpg"></a>
-<p class="block-date">毎日19:00更新</p>
-<p class="block-title"><a href="http://kazenohibiki.blogspot.jp" target="_blank">インターネットラジオ<br>ゴスペル風のひびき<br>心に触れるゴスペルと楽しいおしゃべり 取っ替え引っ替え14組のパーソナリティー</a></p>
 </div>
 
 <div class="column-inner">
