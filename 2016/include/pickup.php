a5628e94a273f4259b03537c75e98066676cd9a9
Samuel N Kasahara <samuel.livebyfaith@gmail.com>
2017-09-20 01:47:45 +0900

    PICKUP修正

--- 2016/include/pickup.php
+++ 2016/include/pickup.php
@@ -55,13 +55,13 @@
 <p class="block-title"><a href="http://antiochblog.jp/info/jerusalem2017/0918hakone/" target="_blank">エルサレム<br>箱根のホテルでロービーコンサート</a></p>
 </div>
 
-<!--<div class="column-inner">
+<div class="column-inner">
 <span class="label-1">ブログ</span>
 <span>NEW</span>
 <a href="http://antiochblog.jp/info/1709gate/" target="_blank"><img src="/2016/images/top/1704gateofhigashikurume.jpg"></a>
 <p class="block-date">日程：2017年9月20日</p>
 <p class="block-title"><a href="http://antiochblog.jp/info/1709gate/" target="_blank">The Gate of Praise<br>練馬区の老人ホームでコンサート</a></p>
-</div>-->
+</div>
 
 <div class="column-inner">
 <span class="label-1">ブログ</span>
