557ef58262e96155bfd3d0e2117d312c4310538e
fujita-PC\fujita <johnfuji4845@gmail.com>
2017-09-29 23:57:20 +0900

    pick up更新(9/30)

--- 2016/include/pickup.php
+++ 2016/include/pickup.php
@@ -14,12 +14,12 @@
 <p class="block-title"><a href="http://www.ustream.tv/channel/tokyoantiochtv" target="_blank" onclick="javascript:pageTracker._trackPageview('カンボジアチーム聖会生中継');">カンボジアチーム聖会生中継<br>(日程：2017年8月22日、23日 日本時間21：00スタート)</a></p>
 </div>-->
 
-<!--<div class="column-inner">
+<div class="column-inner">
 <span class="label-1">ブログ</span>
-<a href="http://astone.tv/events/1709chiba/" target="_blank"><img src="/2016/images/top/1706okinawa.jpg"></a>
+<a href="http://astone.tv/events/1709chiba/" target="_blank"><img src="/2016/images/top/1706chiba.jpg"></a>
 <p class="block-date">日程：2017年9月30日</p>
-<p class="block-title"><a href="http://astone.tv/events/1709chiba/" target="_blank">千葉を行く<br>千葉宣教の旅<br>(日程：2017年9月25日～27日)</a></p>
-</div>-->
+<p class="block-title"><a href="http://astone.tv/events/1709chiba/" target="_blank">千葉を行く<br>千葉宣教の旅<br>(日程：2017年9月30日</a></p>
+</div>
 
 <div class="column-inner">
 <span class="label-1">ブログ</span>
@@ -45,13 +45,21 @@
 <p class="block-title"><a href="http://antiochblog.jp/mission/yosenmae1709/" target="_blank">年末コンサート出場に向かって<br>教会内予選まで一ヶ月</a></p>
 </div>
 
-<!--<div class="column-inner">
+<div class="column-inner">
+<span class="label-1">ブログ</span>
+<span>NEW</span>
+<a href="http://astone.tv/events/1709arakawa/" target="_blank"><img src="/2016/images/top/1706arakawa.jpg"></a>
+<p class="block-date">日程：2017年9月30日</p>
+<p class="block-title"><a href="http://astone.tv/events/1709arakawa/" target="_blank">ラベイユ<br>荒川区の福祉施設でゴスペルコンサート</a></p>
+</div>
+
+<div class="column-inner">
 <span class="label-1">ブログ</span>
 <span>NEW</span>
 <a href="http://antiochblog.jp/praise/gate1709/" target="_blank"><img src="/2016/images/top/1704gateofhigashikurume.jpg"></a>
 <p class="block-date">日程：2017年9月30日</p>
-<p class="block-title"><a href="http://antiochblog.jp/praise/gate1709/" target="_blank">The Gate of Praise<br></a></p>
-</div>-->
+<p class="block-title"><a href="http://antiochblog.jp/praise/gate1709/" target="_blank">The Gate of Praise<br>練馬区の介護老人保健施設にコンサート</a></p>
+</div>
 
 <div class="column-inner">
 <span class="label-1">ブログ</span>
