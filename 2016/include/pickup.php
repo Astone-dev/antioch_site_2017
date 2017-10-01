7284359c4f43e73dfac38f35b319b629a0cfd5be
Samuel N Kasahara <samuel.livebyfaith@gmail.com>
2017-10-02 08:25:12 +0900

    PICKUP修正

--- 2016/include/pickup.php
+++ 2016/include/pickup.php
@@ -17,14 +17,6 @@
 <div class="column-inner">
 <span class="label-1">ブログ</span>
 <span>NEW</span>
-<a href="http://antiochblog.jp/info/pray4jerusalem2017/" target="_blank"><img src="/2016/images/top/1709jerusalemheiwanimukatte.jpg"></a>
-<p class="block-date">日程：2017年9月25日~10月1日</p>
-<p class="block-title"><a href="http://antiochblog.jp/info/pray4jerusalem2017/" target="_blank">いよいよ今日<br>エルサレムの平和を祈る集い<br>スタッフ大忙し<br>(集会日程：2017年10月1日)</a></p>
-</div>
-
-<div class="column-inner">
-<span class="label-1">ブログ</span>
-<span>NEW</span>
 <a href="http://astone.tv/events/chubu1710/" target="_blank"><img src="/2016/images/top/1704chubuseikai.jpg"></a>
 <p class="block-date">日程：2017年10月1日～5日</p>
 <p class="block-title"><a href="http://astone.tv/events/chubu1710/" target="_blank">新秋の中部を行く<br>中部宣教の旅<br>(日程：2017年10月2日～5日)</a></p>
@@ -33,86 +25,64 @@
 <div class="column-inner">
 <span class="label-1">ブログ</span>
 <span>NEW</span>
-<a href="http://antiochblog.jp/praise/autumn1709/" target="_blank"><img src="/2016/images/top/1709akimikakuantioch.jpg"></a>
-<p class="block-date">日程：2017年9月25日~10月1日</p>
-<p class="block-title"><a href="http://antiochblog.jp/praise/autumn1709/" target="_blank">私の秋の味覚 <br>アンテオケ教会</a></p>
-</div>
-
-<!--<div class="column-inner">
-<span class="label-1">ブログ</span>
-<span>NEW</span>
-<a href="http://antiochblog.jp/praise/thanks1710/" target="_blank"><img src="/2016/images/top/1709akimikakuantioch.jpg"></a>
+<a href="http://antiochblog.jp/praise/thanks1710/" target="_blank"><img src="/2016/images/top/1709akiweekantioch.jpg"></a>
 <p class="block-date">日程：2017年10月2日~8日</p>
-<p class="block-title"><a href="http://antiochblog.jp/praise/thanks1710/" target="_blank">秋一番<br>アンテオケ教会</a></p>
-</div>-->
+<p class="block-title"><a href="http://antiochblog.jp/praise/thanks1710/" target="_blank">秋本番<br>アンテオケ教会の一週間</a></p>
+</div>
 
 <div class="column-inner">
 <span class="label-1">ブログ</span>
 <span>NEW</span>
-<a href="http://antiochblog.jp/mission/yosenmae1709/" target="_blank"><img src="/2016/images/top/1709nenmatsuyosenmade.jpg"></a>
-<p class="block-date">日程：2017年9月25日~10月1日</p>
-<p class="block-title"><a href="http://antiochblog.jp/mission/yosenmae1709/" target="_blank">年末コンサート出場に向かって<br>教会内予選まで一ヶ月</a></p>
+<a href="http://antiochblog.jp/info/yosen1710/" target="_blank"><img src="/2016/images/top/1710nenmatsuconcertni.jpg"></a>
+<p class="block-date">日程：2017年10月2日~8日</p>
+<p class="block-title"><a href="http://antiochblog.jp/info/yosen1710/" target="_blank">さあ出るぞ！<br>年末コンサート出場チーム選定<br>足のひっぱりあい</a></p>
 </div>
-
-<!--<div class="column-inner">
-<span class="label-1">ブログ</span>
-<span>NEW</span>
-<a href="http://antiochblog.jp/info/yosen1710/" target="_blank"><img src="/2016/images/top/1709nenmatsuyosenmade.jpg"></a>
-<p class="block-date">日程：2017年10月2日~8日</p>
-<p class="block-title"><a href="http://antiochblog.jp/info/yosen1710/" target="_blank">さあ出るぞ！年末コンサート <br>出場チーム選定 足のひっぱりあい</a></p>
-</div>-->
-
-<!--<div class="column-inner">
-<span class="label-1">ブログ</span>
-<span>NEW</span>
-<a href="http://astone.tv/events/1710kumoniji/" target="_blank"><img src="/2016/images/top/1706kumoniji.jpg"></a>
-<p class="block-date">日程：2017年10月2日~8日</p>
-<p class="block-title"><a href="http://astone.tv/events/1710kumoniji/" target="_blank">雲の間にある虹出版の一週間 <br>  </a></p>
-</div>-->
-
-<!--<div class="column-inner">
-<span class="label-1">ブログ</span>
-<span>NEW</span>
-<a href="http://astone.tv/events/1710mission/" target="_blank"><img src="/2016/images/top/1709nenmatsuyosenmade.jpg"></a>
-<p class="block-date">日程：2017年10月2日~8日</p>
-<p class="block-title"><a href="http://astone.tv/events/1710mission/" target="_blank">聖所から流れ出る水ミッション <br>  </a></p>
-</div>-->
 
 <div class="column-inner">
 <span class="label-1">ブログ</span>
 <span>NEW</span>
-<a href="http://astone.tv/events/autumn1709z/" target="_blank"><img src="/2016/images/top/1709akimikakuzenkoku.jpg"></a>
-<p class="block-date">日程：2017年9月25日~10月1日</p>
-<p class="block-title"><a href="http://astone.tv/events/autumn1709z/" target="_blank">私の秋の味覚<br>全国各教会</a></p>
+<a href="http://astone.tv/events/1710kumoniji/" target="_blank"><img src="/2016/images/top/1710kumonijiweek.jpg"></a>
+<p class="block-date">日程：2017年10月2日~8日</p>
+<p class="block-title"><a href="http://astone.tv/events/1710kumoniji/" target="_blank">雲の間にある虹出版<br>秋の一週間</a></p>
 </div>
 
-<!--<div class="column-inner">
+<div class="column-inner">
 <span class="label-1">ブログ</span>
 <span>NEW</span>
-<a href="http://astone.tv/events/october1710/" target="_blank"><img src="/2016/images/top/1709akimikakuzenkoku.jpg"></a>
+<a href="http://astone.tv/events/1710mission/" target="_blank"><img src="/2016/images/top/1710missionweek.jpg"></a>
 <p class="block-date">日程：2017年10月2日~8日</p>
-<p class="block-title"><a href="http://astone.tv/events/october1710/" target="_blank">秋一番<br>全国各教会</a></p>
-</div>-->
+<p class="block-title"><a href="http://astone.tv/events/1710mission/" target="_blank">ミッションと宣教師たちの<br>秋の一週間</a></p>
+</div>
 
 <div class="column-inner">
+<span class="label-1">ブログ</span>
+<span>NEW</span>
+<a href="http://astone.tv/events/october1710/" target="_blank"><img src="/2016/images/top/1709akiweekzenkoku"></a>
+<p class="block-date">日程：2017年10月2日~8日</p>
+<p class="block-title"><a href="http://astone.tv/events/october1710/" target="_blank">秋本番<br>全国各教会の一週間</a></p>
+</div>
+
+<!-- 
+<div class="column-inner">
+<span class="label-2">映像</span>
+<span>NEW</span>
+<a href="https://youtu.be/zoDPkh42NnQ" target="_blank" onclick="javascript:pageTracker._trackPageview('「地の塩　山室軍平」映画予告編');"><img src="/2016/images/top/1610revivalnightlive.png"></a>
+<p class="block-date">2017年10月21日よりロードショー</p>
+<p class="block-title"><a href="https://youtu.be/zoDPkh42NnQ" target="_blank" onclick="javascript:pageTracker._trackPageview('「地の塩　山室軍平」映画予告編');">「地の塩　山室軍平」映画予告編</a></p>
+</div> -->
+
+<!--<div class="column-inner">
 <span>NEW</span>
 <a href="/pray4jerusalem/" target="_blank"><img src="/2016/images/top/1709peaceforJerusalem.jpg"></a>
 <p class="block-date">日程：2017年10月1日</p>
 <p class="block-title"><a href="http://tokyo.antioch.jp/pray4jerusalem/" target="_blank">エルサレムの平和を祈る集い<br>集会のご案内</a></p>
-</div>
+</div>-->
 
 <div class="column-inner">
 <span>NEW</span>
 <a href="http://tv.antioch.jp/digest/201710-israelcm/" target="_blank"><img src="/2016/images/top/1710israelcm.jpg"></a>
 <p class="block-date">日程：2017年10月30日-11月11日</p>
 <p class="block-title"><a href="http://tv.antioch.jp/digest/201710-israelcm/" target="_blank">2017年11月イスラエルチーム紹介映像</a></p>
-</div>
-
-<div class="column-inner">
-<span>NEW</span>
-<a href="http://tokyo.antioch.jp/tcm/" target="_blank"><img src="/2016/images/top/1709tocharidigest_p.jpg"></a>
-<p class="block-date">日程：2017年9月3日</p>
-<p class="block-title"><a href="http://tokyo.antioch.jp/tcm/" target="_blank">東京カリスマ聖会<br>ダイジェスト写真集<br>(きれいな写真版)</a></p>
 </div>
 
 <div class="column-inner">
@@ -135,15 +105,39 @@
 <p class="block-title"><a href="http://kazenohibiki.blogspot.jp" target="_blank">インターネットラジオ<br>ゴスペル風のひびき<br>心に触れるゴスペルと楽しいおしゃべり 取っ替え引っ替え14組のパーソナリティー</a></p>
 </div>
 
-<!-- 
 <div class="column-inner">
-<span class="label-2">映像</span>
-<span>NEW</span>
-<a href="https://youtu.be/zoDPkh42NnQ" target="_blank" onclick="javascript:pageTracker._trackPageview('「地の塩　山室軍平」映画予告編');"><img src="/2016/images/top/1610revivalnightlive.png"></a>
-<p class="block-date">2017年10月21日よりロードショー</p>
-<p class="block-title"><a href="https://youtu.be/zoDPkh42NnQ" target="_blank" onclick="javascript:pageTracker._trackPageview('「地の塩　山室軍平」映画予告編');">「地の塩　山室軍平」映画予告編</a></p>
-</div> -->
+<span class="label-1">ブログ</span>
+<a href="http://antiochblog.jp/info/pray4jerusalem2017/" target="_blank"><img src="/2016/images/top/1709jerusalemheiwanimukatte.jpg"></a>
+<p class="block-date">日程：2017年9月25日~10月1日</p>
+<p class="block-title"><a href="http://antiochblog.jp/info/pray4jerusalem2017/" target="_blank">いよいよ今日<br>エルサレムの平和を祈る集い<br>スタッフ大忙し<br>(集会日程：2017年10月1日)</a></p>
+</div>
 
+<div class="column-inner">
+<span class="label-1">ブログ</span>
+<a href="http://antiochblog.jp/praise/autumn1709/" target="_blank"><img src="/2016/images/top/1709akimikakuantioch.jpg"></a>
+<p class="block-date">日程：2017年9月25日~10月1日</p>
+<p class="block-title"><a href="http://antiochblog.jp/praise/autumn1709/" target="_blank">私の秋の味覚 <br>アンテオケ教会</a></p>
+</div>
+
+<div class="column-inner">
+<span class="label-1">ブログ</span>
+<a href="http://antiochblog.jp/mission/yosenmae1709/" target="_blank"><img src="/2016/images/top/1709nenmatsuyosenmade.jpg"></a>
+<p class="block-date">日程：2017年9月25日~10月1日</p>
+<p class="block-title"><a href="http://antiochblog.jp/mission/yosenmae1709/" target="_blank">年末コンサート出場に向かって<br>教会内予選まで一ヶ月</a></p>
+</div>
+
+<div class="column-inner">
+<span class="label-1">ブログ</span>
+<a href="http://astone.tv/events/autumn1709z/" target="_blank"><img src="/2016/images/top/1709akimikakuzenkoku.jpg"></a>
+<p class="block-date">日程：2017年9月25日~10月1日</p>
+<p class="block-title"><a href="http://astone.tv/events/autumn1709z/" target="_blank">私の秋の味覚<br>全国各教会</a></p>
+</div>
+
+<div class="column-inner">
+<a href="http://tokyo.antioch.jp/tcm/" target="_blank"><img src="/2016/images/top/1709tocharidigest_p.jpg"></a>
+<p class="block-date">日程：2017年9月3日</p>
+<p class="block-title"><a href="http://tokyo.antioch.jp/tcm/" target="_blank">東京カリスマ聖会<br>ダイジェスト写真集<br>(きれいな写真版)</a></p>
+</div>
 
 <div class="column-inner">
 <span class="label-1">ブログ</span>
