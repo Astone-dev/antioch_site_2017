ca8f11a0f08a2e570df000e86a3b8e51bd3c9e8b
Samuel N Kasahara <samuel.livebyfaith@gmail.com>
2017-10-02 09:02:17 +0900

    PICKUP修正

--- 2016/include/pickup.php
+++ 2016/include/pickup.php
@@ -57,6 +57,14 @@
 <div class="column-inner">
 <span class="label-1">ブログ</span>
 <span>NEW</span>
+<a href="http://antiochblog.jp/info/jerusalem2017/171002kawaguti/" target="_blank"><img src="/2016/images/top/1706jerumisato.jpg"></a>
+<p class="block-date">日程：2017年10月2日</p>
+<p class="block-title"><a href="http://antiochblog.jp/info/jerusalem2017/171002kawaguti/" target="_blank">エルサレム<br>川口のリハビリセンターでゴスペル</a></p>
+</div>
+
+<div class="column-inner">
+<span class="label-1">ブログ</span>
+<span>NEW</span>
 <a href="http://astone.tv/events/october1710/" target="_blank"><img src="/2016/images/top/1710akihonbanzenkoku.jpg"></a>
 <p class="block-date">日程：2017年10月2日～8日</p>
 <p class="block-title"><a href="http://astone.tv/events/october1710/" target="_blank">秋本番<br>全国各教会の一週間</a></p>
