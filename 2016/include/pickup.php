591c0bef58a0313306d3ab517e27c3c478676bf2
Samuel N Kasahara <samuel.livebyfaith@gmail.com>
2017-10-10 00:48:51 +0900

    PICKUP修正

--- 2016/include/pickup.php
+++ 2016/include/pickup.php
@@ -71,14 +71,6 @@
 </div>
 
 <div class="column-inner">
-<span class="label-1">ブログ</span>
-<span>NEW</span>
-<a href="http://antiochblog.jp/mission/1710day/" target="_blank"><img src="/2016/images/top/1705gracioustonai.jpg"></a>
-<p class="block-date">日程：2017年10月9日</p>
-<p class="block-title"><a href="http://antiochblog.jp/mission/1710day/" target="_blank">GRACIOUS  DOORS<br>都内デイサービスでボランティアゴスペルコンサート</a></p>
-</div>
-
-<div class="column-inner">
 <span>NEW</span>
 <a href="http://tokyo.antioch.jp/jfes/" target="_blank"><img src="/2016/images/top/1710ikebukurojfesinfo.jpg"></a>
 <p class="block-date">日程：2017年10月14日</p>
@@ -151,6 +143,13 @@
 <p class="block-date">日程：2017年10月7日</p>
 <p class="block-title"><a href="http://antiochblog.jp/info/jerusalem2017/1007kawaguchi/" target="_blank">エルサレム<br>川口市の施設でゴスペル</a></p>
 </div>-->
+
+<div class="column-inner">
+<span class="label-1">ブログ</span>
+<a href="http://antiochblog.jp/mission/1710day/" target="_blank"><img src="/2016/images/top/1705gracioustonai.jpg"></a>
+<p class="block-date">日程：2017年10月9日</p>
+<p class="block-title"><a href="http://antiochblog.jp/mission/1710day/" target="_blank">GRACIOUS  DOORS<br>都内デイサービスでボランティアゴスペルコンサート</a></p>
+</div>
 
 <div class="column-inner">
 <span class="label-1">ブログ</span>
