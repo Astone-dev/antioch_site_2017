d252bb87a974c544d2ed2bc7cc2de2964ae5b775
keisuke-mori <morikeisuke@morikeisuke.local>
2017-10-11 07:32:12 +0900

    add sp antiochtv page base

--- sp/js/init.js
+++ sp/js/init.js
@@ -1,36 +1,6 @@
 // JavaScript Document
 var TokyoAntioch = TokyoAntioch || {};
 
-var mygallery=new simpleGallery({
-	wrapperid: "simplegallery1", //ID of main gallery container,
-	dimensions: ["100%", "auto"], //width/height of gallery in pixels. Should reflect dimensions of the images exactly
-	imagearray: [
-				/* [?摜URL?A?????N??RL?A_new] */
-				["/2016/images/top/slide/1612ad_jerusalem.jpg","",""],
-				//["/images/gallery/praise.jpg","/features/praise/index.html",""],
-				//["/images/gallery/healing.jpg","/features/healing/index.html",""],
-				//["/images/gallery/gift.jpg","/features/blessing/index.html",""],
-				//["/images/gallery/mission.jpg","/features/mission/index.html",""],
-				//["/images/gallery/women.jpg","/features/women/index.html",""],
-				//["/images/gallery/ophanage.jpg","/features/orphanage/index.html",""],
-				//["/images/gallery/israel.jpg","/features/israel/index.html",""],
-				//["/images/gallery/media.jpg","/features/media/index.html",""],
-				//["/images/gallery/revival.jpg","/features/revival/",""],
-				//["/images/gallery/socialservice.jpg","",""]
-	],
-	autoplay: [false, 4000, 10], //[auto_play_boolean, delay_btw_slide_millisec, cycles_before_stopping_int]
-	persist: false, //remember last viewed slide and recall within same session?
-	fadeduration: 1000, //transition duration (milliseconds)
-	oninit:function(){ //event that fires when gallery has initialized/ ready to run
-		//Keyword "this": references current gallery instance (ie: try this.navigate("play/pause"))
-	},
-	onslide:function(curslide, i){ //event that fires after each slide is shown
-		//Keyword "this": references current gallery instance
-		//curslide: returns DOM reference to current slide's DIV (ie: try alert(curslide.innerHTML)
-		//i: integer reflecting current image within collection being shown (0=1st image, 1=2nd etc)
-	}
-})
-
 // ---------------------------------------------
 // TokyoAntioch.tabClick()
 // メニュー
