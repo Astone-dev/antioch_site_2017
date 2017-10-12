815ed9ac1fe31ca8c83019ffec0defff84b7d5ae
Samuel N Kasahara <samuel.livebyfaith@gmail.com>
2017-10-13 01:23:44 +0900

    PICKUP修正

--- 2016/include/pickup.php
+++ 2016/include/pickup.php
@@ -14,13 +14,13 @@
 <p class="block-title"><a href="http://www.ustream.tv/channel/tokyoantiochtv" target="_blank" onclick="javascript:pageTracker._trackPageview('カンボジアチーム聖会生中継');">カンボジアチーム聖会生中継<br>(日程：2017年8月22日、23日 日本時間21：00スタート)</a></p>
 </div>-->
 
-<!--<div class="column-inner">
+<div class="column-inner">
 <span class="label-1">ブログ</span>
 <span>NEW</span>
-<a href="http://antiochblog.jp/praise/1710meetings/" target="_blank"><img src="/2016/images/top/1708kyusyuseika.jpg"></a>
+<a href="http://antiochblog.jp/praise/1710meetings/" target="_blank"><img src="/2016/images/top/1710kawasaki.jpg"></a>
 <p class="block-date">日程：2017年10月13日～14日</p>
 <p class="block-title"><a href="http://antiochblog.jp/praise/1710meetings/" target="_blank">川崎、豊橋、富士を行く<br>九州宣教の旅<br>(日程：2017年10月13日～14日) </a></p>
-</div>-->
+</div>
 
 <div class="column-inner">
 <span class="label-1">ブログ</span>
