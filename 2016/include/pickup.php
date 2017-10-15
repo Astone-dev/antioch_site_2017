f66f237e642e8f485f01add609abd12b98ff8300
Samuel N Kasahara <samuel.livebyfaith@gmail.com>
2017-10-16 08:36:21 +0900

    PICKUP修正

--- 2016/include/pickup.php
+++ 2016/include/pickup.php
@@ -33,17 +33,17 @@
 <div class="column-inner">
 <span class="label-1">ブログ</span>
 <span>NEW</span>
-<a href="http://antiochblog.jp/praise/joy1710/" target="_blank"><img src="/2016/images/top/1710nenmatsuyorokobi.jpg"></a>
-<p class="block-date">日程：2017年10月16日～22日</p>
-<p class="block-title"><a href="http://antiochblog.jp/praise/joy1710/" target="_blank">GLTVスペシャルコンサート出場決定！<br>喜びと悲しみの一週間</a></p>
+<a href="http://astone.tv/events/1710gifts/" target="_blank"><img src="/2016/images/top/1705giftsmanyuki.jpg"></a>
+<p class="block-date">日程：2017年10月15日～17日</p>
+<p class="block-title"><a href="http://astone.tv/events/1710gifts/" target="_blank">Gifts漫遊記<br>秋の東北を行く2日間<br>(日程：2017年10月16日〜17日)</a></p>
 </div>
 
 <div class="column-inner">
 <span class="label-1">ブログ</span>
 <span>NEW</span>
-<a href="http://astone.tv/events/1710gifts/" target="_blank"><img src="/2016/images/top/1705giftsmanyuki.jpg"></a>
-<p class="block-date">日程：2017年10月15日～17日</p>
-<p class="block-title"><a href="http://astone.tv/events/1710gifts/" target="_blank">Gifts漫遊記<br>秋の東北を行く2日間<br>(日程：2017年10月16日〜17日)</a></p>
+<a href="http://antiochblog.jp/praise/joy1710/" target="_blank"><img src="/2016/images/top/1710nenmatsuyorokobi.jpg"></a>
+<p class="block-date">日程：2017年10月16日～22日</p>
+<p class="block-title"><a href="http://antiochblog.jp/praise/joy1710/" target="_blank">GLTVスペシャルコンサート出場決定！<br>喜びと悲しみの一週間</a></p>
 </div>
 
 <div class="column-inner">
