3091f057a8d2c207c6adc456216d6bf414b9fd37
Samuel N Kasahara <samuel.livebyfaith@gmail.com>
2017-10-20 01:13:41 +0900

    PICKUP修正

--- 2016/include/pickup.php
+++ 2016/include/pickup.php
@@ -41,14 +41,6 @@
 <div class="column-inner">
 <span class="label-1">ブログ</span>
 <span>NEW</span>
-<a href="http://antiochblog.jp/info/jerusalem2017/1019hakone/" target="_blank"><img src="/2016/images/top/1706jerumisato.jpg"></a>
-<p class="block-date">日程：2017年10月19日</p>
-<p class="block-title"><a href="http://antiochblog.jp/info/jerusalem2017/1019hakone/" target="_blank">エルサレム<br>箱根のホテルでロビーコンサート</a></p>
-</div>
-
-<div class="column-inner">
-<span class="label-1">ブログ</span>
-<span>NEW</span>
 <a href="http://antiochblog.jp/praise/uranos2017/category/1020shinyuri/" target="_blank"><img src="/2016/images/top/1705uranosuconcert.jpg"></a>
 <p class="block-date">日程：2017年10月20日</p>
 <p class="block-title"><a href="http://antiochblog.jp/praise/uranos2017/category/1020shinyuri/" target="_blank">ウラノス<br>新百合ヶ丘の老人施設でゴスペルコンサート</a></p>
@@ -60,6 +52,14 @@
 <a href="http://antiochblog.jp/praise/gracejewel2017/category/1020nerima/" target="_blank"><img src="/2016/images/top/1704gracegospel.jpg"></a>
 <p class="block-date">日程：2017年10月20日</p>
 <p class="block-title"><a href="http://antiochblog.jp/praise/gracejewel2017/category/1020nerima/" target="_blank">GRACEと宝石<br>練馬のデイサービスコンサート</a></p>
+</div>
+
+<div class="column-inner">
+<span class="label-1">ブログ</span>
+<span>NEW</span>
+<a href="http://antiochblog.jp/info/jerusalem2017/1019hakone/" target="_blank"><img src="/2016/images/top/1706jerumisato.jpg"></a>
+<p class="block-date">日程：2017年10月19日</p>
+<p class="block-title"><a href="http://antiochblog.jp/info/jerusalem2017/1019hakone/" target="_blank">エルサレム<br>箱根のホテルでロビーコンサート</a></p>
 </div>
 
 <div class="column-inner">
