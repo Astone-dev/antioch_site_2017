96ee898cd31f2082e58935880c7562698f8869c7
Samuel N Kasahara <samuel.livebyfaith@gmail.com>
2017-10-23 09:45:54 +0900

    PICKUP修正

--- 2016/include/pickup.php
+++ 2016/include/pickup.php
@@ -33,6 +33,14 @@
 <div class="column-inner">
 <span class="label-1">ブログ</span>
 <span>NEW</span>
+<a href="http://antiochblog.jp/praise/typhoon1710a/" target="_blank"><img src="/2016/images/top/1710taoifuantioch.jpg"></a>
+<p class="block-date">日程：2017年10月23日～29日</p>
+<p class="block-title"><a href="http://antiochblog.jp/praise/typhoon1710a/" target="_blank">台風を吹きとばせ!<br>東京アンテオケ教会の一週間</a></p>
+</div>
+
+<div class="column-inner">
+<span class="label-1">ブログ</span>
+<span>NEW</span>
 <a href="http://astone.tv/events/arechinikawa1710/" target="_blank"><img src="/2016/images/top/1710arechiweek.jpg"></a>
 <p class="block-date">日程：2017年10月23日～29日</p>
 <p class="block-title"><a href="http://astone.tv/events/arechinikawa1710/" target="_blank">荒地に川ミュージック<br>風のひびき5集歌集発売<br>わくわく一週間</a></p>
