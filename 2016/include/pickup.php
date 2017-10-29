db8321ddedbc89bf15f5a1d9e6252bc01858a408
Samuel N Kasahara <samuel.livebyfaith@gmail.com>
2017-10-30 08:26:46 +0900

    PICKUP修正

--- 2016/include/pickup.php
+++ 2016/include/pickup.php
@@ -25,17 +25,17 @@
 <div class="column-inner">
 <span class="label-1">ブログ</span>
 <span>NEW</span>
-<a href="http://astone.tv/events/typhoon1710/" target="_blank"><img src="/2016/images/top/1611shinnendoantioch.jpg"></a>
+<a href="http://antiochblog.jp/praise/yearend201710/" target="_blank"><img src="/2016/images/top/1611shinnendoantioch.jpg"></a>
 <p class="block-date">日程：2017年10月30日～11月5日</p>
-<p class="block-title"><a href="http://astone.tv/events/typhoon1710/" target="_blank">年度末・新年度<br>東京アンテオケ教会の一週間</a></p>
+<p class="block-title"><a href="http://antiochblog.jp/praise/yearend201710/" target="_blank">年度末・新年度<br>東京アンテオケ教会の一週間</a></p>
 </div>
 
 <div class="column-inner">
 <span class="label-1">ブログ</span>
 <span>NEW</span>
-<a href="http://astone.tv/events/typhoon1710/" target="_blank"><img src="/2016/images/top/1611shinnendozenkoku.jpg"></a>
+<a href="http://astone.tv/events/yearend1710/" target="_blank"><img src="/2016/images/top/1611shinnendozenkoku.jpg"></a>
 <p class="block-date">日程：2017年10月30日～11月5日</p>
-<p class="block-title"><a href="http://astone.tv/events/typhoon1710/" target="_blank">年度末・新年度<br>全国各教会の一週間</a></p>
+<p class="block-title"><a href="http://astone.tv/events/yearend1710/" target="_blank">年度末・新年度<br>全国各教会の一週間</a></p>
 </div>
 
 <div class="column-inner">
