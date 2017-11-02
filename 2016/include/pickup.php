e7687250c69da8beabfc1472fd7f7536f18c9b25
Samuel N Kasahara <samuel.livebyfaith@gmail.com>
2017-11-03 01:10:29 +0900

    PICKUP修正

--- 2016/include/pickup.php
+++ 2016/include/pickup.php
@@ -30,13 +30,13 @@
 <p class="block-title"><a href="http://antiochblog.jp/praise/yearend201710/" target="_blank">年度末・新年度<br>東京アンテオケ教会の一週間</a></p>
 </div>
 
-<!--<div class="column-inner">
+<div class="column-inner">
 <span class="label-1">ブログ</span>
 <span>NEW</span>
-<a href="http://antiochblog.jp/praise/1711gracious/" target="_blank"><img src="/2016/images/top/1711jerpattehhyso.jpg"></a>
+<a href="http://astone.tv/events/1711nakano/" target="_blank"><img src="/2016/images/top/1711jerpattehhyso.jpg"></a>
 <p class="block-date">日程：2017年11月3日</p>
-<p class="block-title"><a href="http://antiochblog.jp/praise/1711gracious/" target="_blank">エルサレム、PATOLIS、テヒリーム、Hyssop<br>中野のイベントでゴスペル</a></p>
-</div>-->
+<p class="block-title"><a href="http://astone.tv/events/1711nakano/" target="_blank">エルサレム、PATOLIS、テヒリーム、Hyssop<br>中野のイベントでゴスペル</a></p>
+</div>
 
 <div class="column-inner">
 <span class="label-1">ブログ</span>
