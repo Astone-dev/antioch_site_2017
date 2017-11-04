362d7b9ef3e1b4629cbd7e1c45956e96652f9f99
fujita-PC\fujita <johnfuji4845@gmail.com>
2017-11-05 07:06:02 +0900

    pick up更新

--- 2016/include/pickup.php
+++ 2016/include/pickup.php
@@ -46,13 +46,13 @@
 <p class="block-title"><a href="http://antiochblog.jp/praise/yearend201710/" target="_blank">年度末・新年度<br>東京アンテオケ教会の一週間</a></p>
 </div>
 
-<!--<div class="column-inner">
+<div class="column-inner">
 <span class="label-1">ブログ</span>
 <span>NEW</span>
 <a href="http://antiochblog.jp/praise/2017labeille/category/1105suginami/" target="_blank"><img src="/2016/images/top/1706labeiyuarakawa.jpg"></a>
 <p class="block-date">日程：2017年11月5日</p>
 <p class="block-title"><a href="http://antiochblog.jp/praise/2017labeille/category/1105suginami/" target="_blank">ラベイユ <br>杉並の音楽祭に出演</a></p>
-</div>-->
+</div>
 
 <div class="column-inner">
 <span class="label-1">ブログ</span>
