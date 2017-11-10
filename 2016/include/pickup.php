2fad42f563f8bcfb738f183b9251be72f80c9d2d
fujita-PC\fujita <johnfuji4845@gmail.com>
2017-11-11 08:16:33 +0900

    pick up更新

--- 2016/include/pickup.php
+++ 2016/include/pickup.php
@@ -81,9 +81,9 @@
 <div class="column-inner">
 <span class="label-1">ブログ</span>
 <span>NEW</span>
-<a href="http://antiochblog.jp/info/jerusalem2017/" target="_blank"><img src="/2016/images/top/1706jerumisato.jpg"></a>
+<a href="http://antiochblog.jp/info/jerusalem2017/1111iwatsuki/" target="_blank"><img src="/2016/images/top/1706jerumisato.jpg"></a>
 <p class="block-date">日程：2017年11月11日～11月12日</p>
-<p class="block-title"><a href="http://antiochblog.jp/info/jerusalem2017/" target="_blank">エルサレム<br>さいたま市岩槻のデイケアでゴスペルコンサート</a></p>
+<p class="block-title"><a href="http://antiochblog.jp/info/jerusalem2017/1111iwatsuki/" target="_blank">エルサレム<br>さいたま市岩槻のデイケアでゴスペルコンサート</a></p>
 </div>
 
 <div class="column-inner">
