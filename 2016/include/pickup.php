1260fbdd89be1d27f1e4ddd9b9f1307edde66deb
Samuel N Kasahara <samuel.livebyfaith@gmail.com>
2017-11-22 01:03:28 +0900

    PICKUP修正

--- 2016/include/pickup.php
+++ 2016/include/pickup.php
@@ -54,13 +54,13 @@
 <p class="block-title"><a href="http://antiochblog.jp/info/1711antioch/" target="_blank">和食党、洋食党の一週間<br>アンテオケ教会</a></p>
 </div>
 
-<!--<div class="column-inner">
+<div class="column-inner">
 <span class="label-1">ブログ</span>
 <span>NEW</span>
 <a href="http://antiochblog.jp/info/jerusalem2017/1122misato/" target="_blank"><img src="/2016/images/top/1706jerumisato.jpg"></a>
 <p class="block-date">日程：2017年11月18日</p>
 <p class="block-title"><a href="http://antiochblog.jp/info/jerusalem2017/1122misato/" target="_blank">エルサレム<br>三郷の福祉施設でゴスペル教室</a></p>
-</div>-->
+</div>
 
 <div class="column-inner">
 <span class="label-1">ブログ</span>
