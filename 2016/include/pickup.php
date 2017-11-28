2a55d09b473ab2ec26be35667c0b041117df7879
fujita-PC\fujita <johnfuji4845@gmail.com>
2017-11-28 23:29:03 +0900

    pick up更新(11/29分)

--- 2016/include/pickup.php
+++ 2016/include/pickup.php
@@ -46,6 +46,14 @@
 <p class="block-title"><a href="http://antiochblog.jp/mission/cornerstone1712/" target="_blank">礎の石孤児院の一週間</a></p>
 </div>
 
+<!--<div class="column-inner">
+<span class="label-1">ブログ</span>
+<span>NEW</span>
+<a href="http://astone.tv/events/1711event/" target="_blank"><img src="/2016/images/top/1711ishizueweek.jpg"></a>
+<p class="block-date">ブログ：2017年11月29日～30日</p>
+<p class="block-title"><a href="http://astone.tv/events/1711event/" target="_blank">渕野浩兄の前夜式、召天式</a></p>
+</div>-->
+
 <div class="column-inner">
 <span class="label-1">ブログ</span>
 <span>NEW</span>
