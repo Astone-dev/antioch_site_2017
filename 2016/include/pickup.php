9703b71ed0f3bd70221d89ccd465a453ce88d233
Samuel N Kasahara <samuel.livebyfaith@gmail.com>
2017-12-06 16:33:42 +0900

    PICKUP修正

--- 2016/include/pickup.php
+++ 2016/include/pickup.php
@@ -57,14 +57,6 @@
 <div class="column-inner">
 <span class="label-1">ブログ</span>
 <span>NEW</span>
-<a href="http://antiochblog.jp/praise/gracejewel2017/1205fuchu/" target="_blank"><img src="/2016/images/top/1708gracehosekifutyu.jpg"></a>
-<p class="block-date">ブログ：2017年12月5日</p>
-<p class="block-title"><a href="http://antiochblog.jp/praise/gracejewel2017/1205fuchu/" target="_blank">GRACEと宝石<br>府中市の特別養護老人ホームでゴスペルコンサート</a></p>
-</div>
-
-<div class="column-inner">
-<span class="label-1">ブログ</span>
-<span>NEW</span>
 <a href="http://astone.tv/events/sanbitai1712/" target="_blank"><img src="/2016/images/top/1712sanbixmaszenkoku.jpg"></a>
 <p class="block-date">ブログ：2017年12月4日～10日</p>
 <p class="block-title"><a href="http://astone.tv/events/sanbitai1712/" target="_blank">ホワイトシーズン賛美隊大爆発<br>全国各教会</a></p>
@@ -117,6 +109,13 @@
 <a href="http://kazenohibiki.blogspot.jp" target="_blank"><img src="/2016/images/top/1604kazenohibikiradio.jpg"></a>
 <p class="block-date">毎日19:00更新</p>
 <p class="block-title"><a href="http://kazenohibiki.blogspot.jp" target="_blank">インターネットラジオ<br>ゴスペル風のひびき<br>心に触れるゴスペルと楽しいおしゃべり 取っ替え引っ替え14組のパーソナリティー</a></p>
+</div>
+
+<div class="column-inner">
+<span class="label-1">ブログ</span>
+<a href="http://antiochblog.jp/praise/gracejewel2017/1205fuchu/" target="_blank"><img src="/2016/images/top/1708gracehosekifutyu.jpg"></a>
+<p class="block-date">ブログ：2017年12月5日</p>
+<p class="block-title"><a href="http://antiochblog.jp/praise/gracejewel2017/1205fuchu/" target="_blank">GRACEと宝石<br>府中市の特別養護老人ホームでゴスペルコンサート</a></p>
 </div>
 
 <div class="column-inner">
