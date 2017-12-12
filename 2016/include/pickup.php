b51a3a778543b2e1881d7731951b77093ed5af8d
Samuel N Kasahara <samuel.livebyfaith@gmail.com>
2017-12-12 10:51:04 +0900

    PICKUP修正

--- 2016/include/pickup.php
+++ 2016/include/pickup.php
@@ -23,6 +23,14 @@
 </div>
 
 <div class="column-inner">
+<span class="label-2">映像</span>
+<span>NEW</span>
+<a href="https://www.youtube.com/watch?v=Kbq3p0d1Wgc" target="_blank" onclick="javascript:pageTracker._trackPageview('アメリカチームデンバー聖会生中継');"><img src="/2016/images/top/1610hanbokushilive.png"></a>
+<p class="block-date">2017年11月5日</p>
+<p class="block-title"><a href="https://www.youtube.com/watch?v=Kbq3p0d1Wgc" target="_blank" onclick="javascript:pageTracker._trackPageview('アメリカチームデンバー聖会生中継');">アメリカチームデンバー聖会生中継<br>日程：2017年12月12日 日本時間10：45頃(時間変更の可能性あり)</a></p>
+</div>-
+
+<div class="column-inner">
 <span class="label-1">ブログ</span>
 <span>NEW</span>
 <a href="http://antiochblog.jp/praise/wintervacation1712/" target="_blank"><img src="/2016/images/top/1712fuyuantioch.jpg"></a>
