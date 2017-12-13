582fb5a3f5219977e96870a25039afca5ccbab28
Samuel N Kasahara <samuel.livebyfaith@gmail.com>
2017-12-14 00:32:26 +0900

    PICKUP修正

--- 2016/include/pickup.php
+++ 2016/include/pickup.php
@@ -49,17 +49,17 @@
 <div class="column-inner">
 <span class="label-1">ブログ</span>
 <span>NEW</span>
-<a href="http://antiochblog.jp/info/jerusalem2017/1214hakone/" target="_blank"><img src="/2016/images/top/1706jerumisato.jpg"></a>
-<p class="block-date">ブログ：2017年12月14日</p>
-<p class="block-title"><a href="http://antiochblog.jp/info/jerusalem2017/1214hakone/" target="_blank">エルサレム<br>箱根のホテルでロビーコンサート</a></p>
+<a href="http://antiochblog.jp/praise/hyssop1712/" target="_blank"><img src="/2016/images/top/1704hyssop.jpg"></a>
+<p class="block-date">ブログ：2017年12月11日～17日</p>
+<p class="block-title"><a href="http://antiochblog.jp/praise/hyssop1712/" target="_blank">ブログアクセス1位<br>ヒソプの一週間</a></p>
 </div>
 
 <div class="column-inner">
 <span class="label-1">ブログ</span>
 <span>NEW</span>
-<a href="http://antiochblog.jp/praise/hyssop1712/" target="_blank"><img src="/2016/images/top/1704hyssop.jpg"></a>
-<p class="block-date">ブログ：2017年12月11日～17日</p>
-<p class="block-title"><a href="http://antiochblog.jp/praise/hyssop1712/" target="_blank">ブログアクセス1位<br>ヒソプの一週間</a></p>
+<a href="http://antiochblog.jp/info/jerusalem2017/1214hakone/" target="_blank"><img src="/2016/images/top/1706jerumisato.jpg"></a>
+<p class="block-date">ブログ：2017年12月14日</p>
+<p class="block-title"><a href="http://antiochblog.jp/info/jerusalem2017/1214hakone/" target="_blank">エルサレム<br>箱根のホテルでロビーコンサート</a></p>
 </div>
 
 <div class="column-inner">
