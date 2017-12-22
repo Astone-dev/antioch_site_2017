3673a80249d0c12796a7ef3ad51df88be1c0124d
fujita-PC\fujita <johnfuji4845@gmail.com>
2017-12-23 00:22:32 +0900

    pick up更新(12/23分)

--- 2016/include/pickup.php
+++ 2016/include/pickup.php
@@ -89,6 +89,14 @@
 <!--<div class="column-inner">
 <span class="label-1">ブログ</span>
 <span>NEW</span>
+<a href="http://astone.tv/events/1712gltv/" target="_blank"><img src="/2016/images/top/1706uranos.jpg"></a>
+<p class="block-date">ブログ：2017年12月23日～24日</p>
+<p class="block-title"><a href="http://astone.tv/events/1712gltv/" target="_blank">GLTV<br></a></p>
+</div>-->
+
+<!--<div class="column-inner">
+<span class="label-1">ブログ</span>
+<span>NEW</span>
 <a href="http://antiochblog.jp/info/1712ramra/" target="_blank"><img src="/2016/images/top/1706uranos.jpg"></a>
 <p class="block-date">ブログ：2017年12月23日</p>
 <p class="block-title"><a href="http://antiochblog.jp/info/1712ramra/" target="_blank">JOY、クワイア<br>飯田橋ラムラでゴスペルコンサート</a></p>
