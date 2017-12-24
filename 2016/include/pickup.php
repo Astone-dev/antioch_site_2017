56cb62b7768d50892174dfb171c8e40a81510ba0
Samuel N Kasahara <samuel.livebyfaith@gmail.com>
2017-12-25 00:31:29 +0900

    PICKUP修正

--- 2016/include/pickup.php
+++ 2016/include/pickup.php
@@ -82,7 +82,15 @@
 <span>NEW</span>
 <a href="/hakuba-synerugoi/" target="_blank"><img src="/2016/images/top/1612hakubacampinfo.jpg"></a>
 <p class="block-date">日程：2018年2月28日(水)～3月2日(金)</p>
-<p class="block-title"><a href="/hakuba-synerugoi/" target="_blank">白馬スネルゴイキャンプ</a></p>
+<p class="block-title"><a href="/hakuba-synerugoi/" target="_blank">白馬スネルゴイキャンプ<br>インフォメーション</a></p>
+</div>
+
+<div class="column-inner">
+<span class="label-2">映像</span>
+<span>NEW</span>
+<a href="http://tv.antioch.jp/digest/201712hakuba-cm/" target="_blank"><img src="/2016/images/top/1712america_d_m.jpg"></a>
+<p class="block-date">日程：2018年2月28日(水)～3月2日(金)</p>
+<p class="block-title"><a href="http://tv.antioch.jp/digest/201712hakuba-cm/" target="_blank">白馬スネルゴイキャンプ<br>紹介映像<br>(再生時間 5:09)</a></p>
 </div>
 
 <div class="column-inner">
