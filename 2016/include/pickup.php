494fa084f2abf7302c04a7576c0d4c5054385731
Samuel N Kasahara <samuel.livebyfaith@gmail.com>
2017-12-29 12:45:34 +0900

    PICKUP修正

--- 2016/include/pickup.php
+++ 2016/include/pickup.php
@@ -22,13 +22,13 @@
 <p class="block-title"><a href="https://www.youtube.com/watch?v=HJ3LpHn4ax4" target="_blank" onclick="javascript:pageTracker._trackPageview('アメリカチーム・デンバー聖会生中継');">アメリカチーム・ニューヨーク聖会生中継<br>日程：2017年12月13日 日本時間7：00頃(時間変更の可能性あり)</a></p>
 </div>-->
 
-<!--<div class="column-inner">
+<div class="column-inner">
 <span class="label-1">ブログ</span>
 <span>NEW</span>
-<a href="http://antiochblog.jp/info/1712cambodia/" target="_blank"><img src="/2016/images/top/1712nenmatsuantioch.jpg"></a>
+<a href="http://antiochblog.jp/info/1712cambodia/" target="_blank"><img src="/2016/images/top/1708cambodia_blog.jpg"></a>
 <p class="block-date">ブログ：2017年12月29日～</p>
 <p class="block-title"><a href="http://antiochblog.jp/info/1712cambodia/" target="_blank">カンボジアチーム直前情報</a></p>
-</div>-->
+</div>
 
 <div class="column-inner">
 <span class="label-1">ブログ</span>
