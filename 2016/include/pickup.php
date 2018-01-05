ff2e81f8e3887d10d360677e5d34eee33bfdd59b
Timothy Obata <timo77@antioch.jp>
2018-01-05 23:45:43 +0900

    ブログ：2017年1月 -> 2018年1月 へ修正

--- 2016/include/pickup.php
+++ 2016/include/pickup.php
@@ -38,12 +38,12 @@
 <p class="block-title"><a href="https://www.youtube.com/channel/UCOV9zn7vX4O98uRzWlODMew" target="_blank" onclick="javascript:pageTracker._trackPageview('カンボジアチーム聖会生中継');">カンボジアチーム聖会生中継<br>日程：2018年1月3日 日本時間9：00頃<br>日程：2018年1月4日 日本時間9：00頃<br></a>
 <i style="color:#ff0000;font-size: 11px;">※ページ内のライブ配信中をタップしてごらんください</i></p>
 </div>-->
-
+ 
 <div class="column-inner">
 <span class="label-1">ブログ</span>
 <span>NEW</span>
 <a href="http://antiochblog.jp/info/newyear2018/" target="_blank"><img src="/2016/images/top/1701syougatuantioch.jpg"></a>
-<p class="block-date">ブログ：2017年1月1日～7日</p>
+<p class="block-date">ブログ：2018年1月1日～7日</p>
 <p class="block-title"><a href="http://antiochblog.jp/info/newyear2018/" target="_blank">2018年お正月の一週間<br>東京アンテオケ教会</a></p>
 </div>
 
@@ -51,7 +51,7 @@
 <span class="label-1">ブログ</span>
 <span>NEW</span>
 <a href="http://astone.tv/events/jfes1801/" target="_blank"><img src="/2016/images/top/1712nenmatsuzenkoku.jpg"></a>
-<p class="block-date">ブログ：2017年1月1日～8日</p>
+<p class="block-date">ブログ：2018年1月1日～8日</p>
 <p class="block-title"><a href="http://astone.tv/events/jfes1801/" target="_blank">渋谷J.GOSPEL FESに向かって出演グループとThe Visionスタッフの一週間<br>(日程：2018年1月8日)</a></p>
 </div>
 
@@ -59,7 +59,7 @@
 <span class="label-1">ブログ</span>
 <span>NEW</span>
 <a href="http://astone.tv/events/newyear2018z/" target="_blank"><img src="/2016/images/top/1701syougatuzenkoku.jpg"></a>
-<p class="block-date">ブログ：2017年1月1日～7日</p>
+<p class="block-date">ブログ：2018年1月1日～7日</p>
 <p class="block-title"><a href="http://astone.tv/events/newyear2018z/" target="_blank">2018年お正月の一週間<br>全国各教会</a></p>
 </div>
 
