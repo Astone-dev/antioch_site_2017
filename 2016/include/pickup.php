ba779d5b063b0df6a8a5ab5decb75a86fb7d27ac
fujita-PC\fujita <johnfuji4845@gmail.com>
2018-01-18 19:19:56 +0900

    pick up 更新

--- 2016/include/pickup.php
+++ 2016/include/pickup.php
@@ -23,13 +23,13 @@
 <i style="color:#ff0000;font-size: 11px;">※ページ内のライブ配信中をタップしてごらんください</i></p>
 </div>-->
 
-<div class="column-inner">
+<!--<div class="column-inner">
 <span class="label-1">ブログ</span>
 <span>NEW</span>
-<a href="http://antiochblog.jp/info/chubu1801/" target="_blank"><img src="/2016/images/top/1801chubu.jpg"></a>
-<p class="block-date">ブログ：2018年1月14日～18日</p>
-<p class="block-title"><a href="http://antiochblog.jp/info/chubu1801/" target="_blank">冬の日差しの中<br>中部宣教の旅<br> (日程：2018年1月15日～17日)</a></p>
-</div>
+<a href="http://antiochblog.jp/info/1801meetings/" target="_blank"><img src="/2016/images/top/1801chubu.jpg"></a>
+<p class="block-date">ブログ：2018年1月19日～20日</p>
+<p class="block-title"><a href="http://antiochblog.jp/info/1801meetings/" target="_blank">冬の日差しの中<br>船橋、大和宣教の旅<br> (日程：2018年1月19日～20日)</a></p>
+</div>-->
 
 <!--<div class="column-inner">
 <span class="label-2">映像</span>
@@ -55,13 +55,13 @@
 <p class="block-title"><a href="http://astone.tv/events/yosen1801/" target="_blank">J.GOSPELコンテスト in 白馬<br>東京アンテオケ予選に向かって<br>ドキドキの一週間<br>(日程：2018年1月21日)</a></p>
 </div>
 
-<div class="column-inner">
+<!--<div class="column-inner">
 <span class="label-1">ブログ</span>
 <span>NEW</span>
-<a href="http://antiochblog.jp/info/jerusalem2017/180117misato/" target="_blank"><img src="/2016/images/top/1705jerumisato.jpg"></a>
-<p class="block-date">ブログ：2018年1月17日</p>
-<p class="block-title"><a href="http://antiochblog.jp/info/jerusalem2017/180117misato/" target="_blank">エルサレム<br>埼玉三郷の施設でゴスペル教室<br></a></p>
-</div>
+<a href="http://antiochblog.jp/info/jerusalem2017/180120misato/" target="_blank"><img src="/2016/images/top/1705jerumisato.jpg"></a>
+<p class="block-date">ブログ：2018年1月20日</p>
+<p class="block-title"><a href="http://antiochblog.jp/info/jerusalem2017/180120misato/" target="_blank">エルサレム<br>さいたま市岩槻のデイケアでゴスペルコンサート<br></a></p>
+</div>-->
 
 <div class="column-inner">
 <span class="label-1">ブログ</span>
@@ -131,6 +131,20 @@
 <a href="http://kazenohibiki.blogspot.jp" target="_blank"><img src="/2016/images/top/1604kazenohibikiradio.jpg"></a>
 <p class="block-date">毎日19:00更新</p>
 <p class="block-title"><a href="http://kazenohibiki.blogspot.jp" target="_blank">インターネットラジオ<br>ゴスペル風のひびき<br>心に触れるゴスペルと楽しいおしゃべり 取っ替え引っ替え14組のパーソナリティー</a></p>
+</div>
+
+<div class="column-inner">
+<span class="label-1">ブログ</span>
+<a href="http://antiochblog.jp/info/chubu1801/" target="_blank"><img src="/2016/images/top/1801chubu.jpg"></a>
+<p class="block-date">ブログ：2018年1月14日～18日</p>
+<p class="block-title"><a href="http://antiochblog.jp/info/chubu1801/" target="_blank">冬の日差しの中<br>中部宣教の旅<br> (日程：2018年1月15日～17日)</a></p>
+</div>
+
+<div class="column-inner">
+<span class="label-1">ブログ</span>
+<a href="http://antiochblog.jp/info/jerusalem2017/180117misato/" target="_blank"><img src="/2016/images/top/1705jerumisato.jpg"></a>
+<p class="block-date">ブログ：2018年1月17日</p>
+<p class="block-title"><a href="http://antiochblog.jp/info/jerusalem2017/180117misato/" target="_blank">エルサレム<br>埼玉三郷の施設でゴスペル教室<br></a></p>
 </div>
 
 <div class="column-inner">
