08531e797bb02c82051a82600a466c312421b816
fujita-PC\fujita <johnfuji4845@gmail.com>
2018-01-19 11:18:23 +0900

    pick up更新(1/20分)

--- 2016/include/pickup.php
+++ 2016/include/pickup.php
@@ -64,6 +64,22 @@
 </div>
 
 <!--<div class="column-inner">
+<span class="label-2">映像</span>
+<span>NEW</span>
+<a href="https://www.youtube.com/channel/UCOV9zn7vX4O98uRzWlODMew/live" target="_blank" onclick="javascript:pageTracker._trackPageview('Revival Night生中継'); "><img src="/2016/images/top/1610revivalnightlive.png"></a>
+<p class="block-date">2017年12月16日</p>
+<p class="block-title"><a href="https://www.youtube.com/channel/UCOV9zn7vX4O98uRzWlODMew/live" target="_blank" onclick="javascript:pageTracker._trackPageview('Revival Night生中継');">Revival Night生中継<br>(日程：2017年12月16日 18:30スタート)</a></p>
+</div>-->
+
+<!--<div class="column-inner">
+<span class="label-1">ブログ</span>
+<span>NEW</span>
+<a href="http://antiochblog.jp/info/revival2017/180120night/" target="_blank"><img src="/2016/images/top/1705revivalnight.jpg"></a>
+<p class="block-date">ブログ：2018年1月20日</p>
+<p class="block-title"><a href="http://antiochblog.jp/info/revival2017/180120night/" target="_blank">1月 Revival Night<br></a></p>
+</div>-->
+
+<!--<div class="column-inner">
 <span class="label-1">ブログ</span>
 <span>NEW</span>
 <a href="http://antiochblog.jp/info/jerusalem2017/180120misato/" target="_blank"><img src="/2016/images/top/1705jerumisato.jpg"></a>
