6c92dc1bfab323de5d505e7da1f10ac98807ffd9
Samuel N Kasahara <samuel.livebyfaith@gmail.com>
2018-02-12 07:51:00 +0900

    PICKUP修正

--- 2016/include/pickup.php
+++ 2016/include/pickup.php
@@ -42,9 +42,9 @@
 <div class="column-inner">
 <span class="label-1">ブログ</span>
 <span>NEW</span>
-<a href="http://antiochblog.jp/praise/myanmar1802/" target="_blank"><img src="/2016/images/top/1802sunerugoinimukateantioch.jpg"></a>
+<a href="http://antiochblog.jp/praise/myanmar1802/" target="_blank"><img src="/2016/images/top/1802myanma.jpg"></a>
 <p class="block-date">ブログ：2018年2月12日</p>
-<p class="block-title"><a href="http://antiochblog.jp/praise/myanmar1802/" target="_blank">ミャンマー人教会</a></p>
+<p class="block-title"><a href="http://antiochblog.jp/praise/myanmar1802/" target="_blank">ミャンマー教会での奉仕<br>Gifts、Tree of Life</a></p>
 </div>
 
 <div class="column-inner">
