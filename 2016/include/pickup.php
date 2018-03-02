1367b4a351a02d94271abcae5dd5ac6ab6899458
Samuel N Kasahara <samuel.livebyfaith@gmail.com>
2018-03-03 00:04:34 +0900

    PICKUP修正

--- 2016/include/pickup.php
+++ 2016/include/pickup.php
@@ -34,9 +34,9 @@
 <div class="column-inner">
 <span class="label-1">ブログ</span>
 <span>NEW</span>
-<a href="http://astone.tv/events/chiba1803/" target="_blank"><img src="/2016/images/top/1803tcm.jpg"></a>
-<p class="block-date">ブログ：2018年3月3日</p>
-<p class="block-title"><a href="http://astone.tv/events/chiba1803/" target="_blank">春近し<br>東京カリスマ聖会<br>スタッフのドタバタ<br>日程：2018年3月3日</a></p>
+<a href="http://antiochblog.jp/info/tcm1803/" target="_blank"><img src="/2016/images/top/1803tcm.jpg"></a>
+<p class="block-date">ブログ：2018年3月3日〜4日</p>
+<p class="block-title"><a href="http://antiochblog.jp/info/tcm1803/" target="_blank">春近し<br>東京カリスマ聖会<br>スタッフのドタバタ<br>日程：2018年3月4日</a></p>
 </div>
 
 <div class="column-inner">
