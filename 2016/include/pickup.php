0bc8d43135daba2bc62494add07c852f1d5299f3
Samuel N Kasahara <samuel.livebyfaith@gmail.com>
2018-03-16 06:36:31 +0900

    PICKUP修正

--- 2016/include/pickup.php
+++ 2016/include/pickup.php
@@ -34,14 +34,6 @@
 <div class="column-inner">
 <span class="label-1">ブログ</span>
 <span>NEW</span>
-<a href="http://blog.antioch.jp/1803hawaii/" target="_blank"><img src="/2016/images/top/1803hawaiiteam.jpg"></a>
-<p class="block-date">日程：2018年3月12日～15日</p>
-<p class="block-title"><a href="http://blog.antioch.jp/1803hawaii/" target="_blank">ハワイチーム<br>最新情報写真集<br>(チーム日程：2018年3月12日～15日)</a></p>
-</div>
-
-<div class="column-inner">
-<span class="label-1">ブログ</span>
-<span>NEW</span>
 <a href="http://antiochblog.jp/info/1803kanagawa/" target="_blank"><img src="/2016/images/top/1702yokohamaseikai.jpg"></a>
 <p class="block-date">日程：2018年3月16日〜17日</p>
 <p class="block-title"><a href="http://antiochblog.jp/info/1803kanagawa/" target="_blank">春の光の中横浜、横須賀聖会を行く<br>関東宣教の旅<br>日程：2018年3月16日〜17日</a></p>
@@ -162,6 +154,13 @@
 <a href="http://tokyo.antioch.jp/yorimichi/result-standup.html" target="_blank"><img src="/2016/images/top/1709yorimichi28kekka.jpg"></a>
 <p class="block-title"><a href="http://tokyo.antioch.jp/yorimichi/result-standup.html" target="_blank">ついに決定！<br>クリスチャンの寄り道 Vol.28<br>「倒れて立ち上がった経験」のお話し人気ベスト10<br>ぜひお聴きください。</a></p>
 </div>-->
+
+<div class="column-inner">
+<span class="label-1">ブログ</span>
+<a href="http://blog.antioch.jp/1803hawaii/" target="_blank"><img src="/2016/images/top/1803hawaiiteam.jpg"></a>
+<p class="block-date">日程：2018年3月12日～15日</p>
+<p class="block-title"><a href="http://blog.antioch.jp/1803hawaii/" target="_blank">ハワイチーム<br>最新情報写真集<br>(チーム日程：2018年3月12日～15日)</a></p>
+</div>
 
 <div class="column-inner">
 <span class="label-1">ブログ</span>
