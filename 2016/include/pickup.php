df4720cf9194f2539a4229c2b025a167879c7b96
Samuel N Kasahara <samuel.livebyfaith@gmail.com>
2018-03-17 01:53:53 +0900

    PICKUP修正

--- 2016/include/pickup.php
+++ 2016/include/pickup.php
@@ -75,7 +75,7 @@
 <div class="column-inner">
 <span class="label-1">ブログ</span>
 <span>NEW</span>
-<a href="http://antiochblog.jp/info/revival2017/180317night/" target="_blank"><img src="/2016/images/top/1703graduationantioch.jpg"></a>
+<a href="http://antiochblog.jp/info/revival2017/180317night/" target="_blank"><img src="/2016/images/top/1703revivalnight.jpg"></a>
 <p class="block-date">日程：2018年3月17日</p>
 <p class="block-title"><a href="http://antiochblog.jp/info/revival2017/180317night/" target="_blank">3月Revival Nightの一日</a></p>
 </div>
