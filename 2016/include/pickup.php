3fb0a4cf127bd6f25d14bb77d1e9ef88137126bf
Samuel N Kasahara <samuel.livebyfaith@gmail.com>
2018-03-19 01:08:43 +0900

    PICKUP修正

--- 2016/include/pickup.php
+++ 2016/include/pickup.php
@@ -26,7 +26,7 @@
 <<div class="column-inner">
 <span class="label-1">ブログ</span>
 <span>NEW</span>
-<a href="http://antiochblog.jp/info/2yobi1803/" target="_blank"><img src="/2016/images/top/1703sakuraantioch.jpg"></a>
+<a href="http://antiochblog.jp/praise/faith1803/" target="_blank"><img src="/2016/images/top/1703sakuraantioch.jpg"></a>
 <p class="block-date">日程：2018年3月19日～25日</p>
 <p class="block-title"><a href="http://antiochblog.jp/praise/faith1803/" target="_blank">桜の咲くころ<br>アンテオケ教会のさわやか一週間</a></p>
 </div>
