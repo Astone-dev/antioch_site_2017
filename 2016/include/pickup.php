251a520ac291fe066093119c6d2cf2c7c7f5f90b
a-hisa0830 <johnfuji4845@gmail.com>
2018-03-23 23:06:00 +0900

    pick up更新

--- 2016/include/pickup.php
+++ 2016/include/pickup.php
@@ -47,21 +47,21 @@
 <p class="block-title"><a href="http://astone.tv/events/1803nerima/" target="_blank">JOY <br>阿佐ヶ谷ドラムでコンサート</a></p>
 </div>
 
-<!--<div class="column-inner">
+<div class="column-inner">
 <span class="label-1">ブログ</span>
 <span>NEW</span>
 <a href="http://antiochblog.jp/praise/gospel2017/180324gospel/" target="_blank"><img src="/2016/images/top/1709gospeltudoi.jpg"></a>
 <p class="block-date">日程：2018年3月24日</p>
 <p class="block-title"><a href="http://antiochblog.jp/praise/gospel2017/180324gospel/" target="_blank">3月 ゴスペルの集い</a></p>
-</div>-->
+</div>
 
-<!--<div class="column-inner">
+<div class="column-inner">
 <span class="label-2">映像</span>
 <span>NEW</span>
 <a href="https://www.youtube.com/channel/UCOV9zn7vX4O98uRzWlODMew/live" target="_blank" onclick="javascript:pageTracker._trackPageview('ゴスペルの集い-生中継'); "><img src="/2016/images/top/1610hanbokushilive.png"></a>
 <p class="block-date">2018年3月24日 18:30～</p>
 <p class="block-title"><a href="https://www.youtube.com/channel/UCOV9zn7vX4O98uRzWlODMew/live" target="_blank" onclick="javascript:pageTracker._trackPageview('ゴスペルの集い-生中継');">ゴスペルの集い 生中継<br>(日程：2017年12月23日 18:30スタート)</a></p>
-</div>-->
+</div>
 
 <div class="column-inner">
 <span>NEW</span>
@@ -86,13 +86,13 @@
 <p class="block-title"><a href="https://www.instagram.com/tlcccmovielog/" target="_blank" onclick="javascript:pageTracker._trackPageview('短い動画で見る春を感じてほのぼのとTLCCC全国各教会');">短い動画で見る<br>春を感じてほのぼのと<br>TLCCC全国各教会</p>
 </div>
 
-<!--<div class="column-inner">
+<div class="column-inner">
 <span class="label-1">ブログ</span>
 <span>NEW</span>
 <a href="http://antiochblog.jp/praise/1803gate/" target="_blank"><img src="/2016/images/top/1704gateofhigashikurume.jpg"></a>
 <p class="block-date">日程：2018年3月24日</p>
 <p class="block-title"><a href="http://antiochblog.jp/praise/1803gate/" target="_blank">The Gate of Praise<br>練馬区の老健でゴスペルコンサート</a></p>
-</div>-->
+</div>
 
 <div class="column-inner">
 <span>NEW</span>
