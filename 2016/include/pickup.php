c953303b026096436cac7b227b4b3a9715a6f6d0
a-hisa0830 <johnfuji4845@gmail.com>
2018-03-31 15:38:06 +0900

    pick up削除

--- 2016/include/pickup.php
+++ 2016/include/pickup.php
@@ -16,15 +16,6 @@
 </div>
 
 <div class="column-inner">
-<span class="label-2">映像</span>
-<span>NEW</span>
-<a href="https://www.youtube.com/channel/UCOV9zn7vX4O98uRzWlODMew" target="_blank" onclick="javascript:pageTracker._trackPageview('感謝と賛美とリバイバルキャンプ生中継');"><img src="/2016/images/top/1610hanbokushilive.png"></a>
-<p class="block-date">2018年3月27日〜30日</p>
-<p class="block-title"><a href="https://www.youtube.com/channel/UCOV9zn7vX4O98uRzWlODMew" target="_blank" onclick="javascript:pageTracker._trackPageview('ハワイチーム聖会生中継');">感謝と賛美とリバイバルキャンプ生中継<br>3月27日〜29日 日本時間11:00頃<br>3月27日〜30日　日本時間22:30頃<br></a>
-<i style="color:#ff0000;font-size: 11px;">※ページ内のライブ配信中をタップしてごらんください</i></p>
-</div>
-
-<div class="column-inner">
 <span class="label-1">ブログ</span>
 <span>NEW</span>
 <a href="http://antiochblog.jp/praise/thanks1803/" target="_blank"><img src="/2016/images/top/1803cherry_a.jpg"></a>
